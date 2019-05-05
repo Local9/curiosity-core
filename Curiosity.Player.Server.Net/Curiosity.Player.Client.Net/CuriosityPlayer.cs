4f174c7100574eaa29f2dc62c78f946883125f7f
Antony Cook <kinglo0py@gmail.com>
2019-05-05 18:10:29 +0100

    test notification

---
+++ Curiosity.Player.Server.Net/Curiosity.Player.Client.Net/CuriosityPlayer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using CitizenFX.Core;
+using CitizenFX.Core.UI;
+using CitizenFX.Core.Native;
+
+namespace Curiosity.Player.Client.Net
+{
+    public class CuriosityPlayer : BaseScript
+    {
+        public CuriosityPlayer()
+        {
+            EventHandlers["onResourceStart"] += new Action<string>(OnResourceStart);
+        }
+
+        private void OnResourceStart(string resourceName)
+        {
+            if (API.GetCurrentResourceName() != resourceName) return;
+
+            Debug.WriteLine("curiosity-server -> Started");
+
+            Screen.ShowNotification("~b~Info:~w~ Curiosity Server Started");
+        }
+    }
+}
---
+++ Curiosity.Player.Server.Net/Curiosity.Player.Server.Net/CuriosityPlayer.cs
@@ -0,0 +1,21 @@
+﻿using CitizenFX.Core;
+using CitizenFX.Core.Native;
+using System;
+
+namespace Curiosity.Player.Server.Net
+{
+    public class CuriosityPlayer : BaseScript
+    {
+        public CuriosityPlayer()
+        {
+            EventHandlers["onResourceStart"] += new Action<string>(OnResourceStart);
+        }
+
+        private void OnResourceStart(string resourceName)
+        {
+            if (API.GetCurrentResourceName() != resourceName) return;
+
+            Debug.WriteLine("curiosity-server -> Started");
+        }
+    }
+}
