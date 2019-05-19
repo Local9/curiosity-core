1d8a9ed3d750e84be33d543f144e288f27bff740
Antony Cook <kinglo0py@gmail.com>
2019-05-19 19:31:14 +0100

    Added mugshot class

---
+++ Curiosity/Curiosity.Player.Client.Net/Classes/Player/MugshotCreator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Curiosity.Client.net.Classes.Player
+{
+    static class MugshotCreator
+    {
+        public static void Init()
+        {
+            // https://forum.fivem.net/t/c-render-target-example-using-mugshot-sign/180682
+        }
+    }
+}
