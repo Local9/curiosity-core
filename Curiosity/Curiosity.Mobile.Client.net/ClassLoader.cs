109362f813a159d7f087471ac234d6bcfeba8825
Antony Cook <kinglo0py@gmail.com>
2019-07-19 22:55:02 +0100

    added mobile project

---
+++ Curiosity/Curiosity.Mobile.Client.net/ClassLoader.cs
@@ -0,0 +1,23 @@
+﻿using Curiosity.Shared.Client.net;
+
+namespace Curiosity.Mobile.Client.net
+{
+    /// <summary>
+    /// For initialization of all these static classes
+    /// May want to split this into multiple more specific files later
+    /// </summary>
+    static class ClassLoader
+    {
+        public static void Init()
+        {
+            Log.Verbose("Entering ClassLoader Init");
+
+            // Mobile Phone Addons
+            //Classes.Environment.UI.Mobile.MobilePhone.Init();
+            //Classes.Environment.UI.Mobile.Apps.PlayerListApplication.Init();
+            //Classes.Environment.UI.Mobile.Apps.SettingsApplication.Init();
+
+            Log.Verbose("Leaving ClassLoader Init");
+        }
+    }
+}
---
+++ Curiosity/Curiosity.Mobile.Client.net/Client.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using CitizenFX.Core;
+using CitizenFX.Core.Native;
+using Curiosity.Shared.Client.net;
+using Curiosity.Shared.Client.net.Models;
+using GlobalEntity = Curiosity.Global.Shared.net.Entity;
+
+namespace Curiosity.Mobile.Client.net
+{
+    public class Client : BaseScript
+    {
+        private static Client _instance;
+        public static PlayerList players;
+
+        public static Client GetInstance()
+        {
+            return _instance;
+        }
+
+        public Client()
+        {
+            _instance = this;
+
+            players = Players;
+
+            ClassLoader.Init();
+
+            Log.Info("Curiosity.Mobile.Client.net loaded\n");
+        }
+    }
+}
