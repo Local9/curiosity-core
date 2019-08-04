3a27fb0faabefabf6293c714290da679543c0dac
Antony Cook <kinglo0py@gmail.com>
2019-08-05 00:30:44 +0100

    lowered xp rate

--- Curiosity/Curiosity.Police.Client.net/Classes/CreateShopCallout.cs
+++ Curiosity/Curiosity.Police.Client.net/Classes/CreateShopCallout.cs
@@ -228,7 +228,7 @@ namespace Curiosity.Police.Client.net.Classes
                 }
 
                 Client.TriggerServerEvent("curiosity:Server:Bank:IncreaseCash", Player.PlayerInformation.playerInfo.Wallet, random.Next(100, 200));
-                Client.TriggerServerEvent("curiosity:Server:Skills:Increase", $"{Enums.Skills.policexp}", 100);
+                Client.TriggerServerEvent("curiosity:Server:Skills:Increase", $"{Enums.Skills.policexp}", random.Next(10, 16));
 
                 ShopKeeper.MarkAsNoLongerNeeded();
                 await Client.Delay(10);
