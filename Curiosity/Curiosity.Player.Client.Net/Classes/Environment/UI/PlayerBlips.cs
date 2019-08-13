ff3c9c2080796d78477027dccefe9e42a71c8059
Antony Cook <kinglo0py@gmail.com>
2019-08-13 23:59:21 +0100

    add blip grouping

--- Curiosity/Curiosity.Player.Client.Net/Classes/Environment/UI/PlayerBlips.cs
+++ Curiosity/Curiosity.Player.Client.Net/Classes/Environment/UI/PlayerBlips.cs
@@ -206,6 +206,7 @@ namespace Curiosity.Client.net.Classes.Environment.UI
                     API.SetBlipRotation(blip, (int)Math.Ceiling(API.GetEntityHeading(vehicleHandle)));
                     API.SetBlipNameToPlayerName(blip, playerHandle);
                     API.SetBlipScale(blip, 0.85f);
+                    API.SetBlipCategory(blip, 7);
 
                     if (API.IsPauseMenuActive())
                     {
