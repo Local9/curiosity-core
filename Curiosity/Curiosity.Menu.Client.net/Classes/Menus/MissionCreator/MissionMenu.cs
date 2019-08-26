edfad14d8ded329776e3aa8be0e00b77be8fd295
Antony Cook <kinglo0py@gmail.com>
2019-08-26 10:31:18 +0100

    mission marker needs planning

---
+++ Curiosity/Curiosity.Menu.Client.net/Classes/Menus/MissionCreator/MissionMenu.cs
@@ -0,0 +1,19 @@
+﻿using MenuAPI;
+
+namespace Curiosity.Menus.Client.net.Classes.Menus.MissionCreator
+{
+    class MissionMenu
+    {
+        static Client client = Client.GetInstance();
+        static Menu menu = new Menu("Mission Maker", "WORK IN PROGRESS");
+
+        public static void Init()
+        {
+            if (Player.PlayerInformation.IsDeveloper())
+            {
+
+                MenuBase.AddSubMenu(menu);
+            }
+        }
+    }
+}
