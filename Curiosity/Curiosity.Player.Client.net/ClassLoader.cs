35af95689d1a140d800310ddd37ccfd0d3cba079
Antony Cook <kinglo0py@gmail.com>
2019-09-06 21:58:34 +0100

    initial player resource

---
+++ Curiosity/Curiosity.Player.Client.net/ClassLoader.cs
@@ -0,0 +1,28 @@
+﻿using Curiosity.Shared.Client.net;
+using System;
+
+namespace Curiosity.Player.Client.net
+{
+    /// <summary>
+    /// For initialization of all these static classes
+    /// May want to split this into multiple more specific files later
+    /// </summary>
+    static class ClassLoader
+    {
+        static Client client = Client.GetInstance();
+
+        public static void Init()
+        {
+            Log.Verbose("Entering ClassLoader Init");
+
+            // Player Spawn
+            // Player Respawn
+            // Player Roles
+            // Player Wallet
+            // Player Skills
+            // Player Shit in General
+
+            Log.Verbose("Leaving ClassLoader Init");
+        }
+    }
+}
---
+++ Curiosity/Curiosity.Player.Client.net/Client.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CitizenFX.Core;
+using CitizenFX.Core.Native;
+using Curiosity.Shared.Client.net;
+
+namespace Curiosity.Player.Client.net
+{
+    public class Client : BaseScript
+    {
+        private static Client _instance;
+        public static PlayerList players;
+
+        public static Client GetInstance()
+        {
+            return _instance;
+        }
+
+        public Client()
+        {
+            _instance = this;
+
+            players = Players;
+
+            //ClassLoader.Init();
+
+            Log.Info("Curiosity.Player.Client.net loaded\n");
+        }
+
+        /// <summary>
+        /// Registers a network event
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="action"></param>
+        public void RegisterEventHandler(string name, Delegate action)
+        {
+            try
+            {
+                EventHandlers[name] += action;
+            }
+            catch (Exception ex)
+            {
+                Log.Error(ex.Message);
+            }
+        }
+
+        /// <summary>
+        /// Registers a NUI/CEF event
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="action"></param>
+        public void RegisterNuiEventHandler(string name, Delegate action)
+        {
+            try
+            {
+                Function.Call(Hash.REGISTER_NUI_CALLBACK_TYPE, name);
+                RegisterEventHandler(string.Concat("__cfx_nui:", name), action);
+            }
+            catch (Exception ex)
+            {
+                Log.Error(ex.Message);
+            }
+        }
+
+        /// <summary>
+        /// Registers a tick function
+        /// </summary>
+        /// <param name="action"></param>
+        public void RegisterTickHandler(Func<Task> action)
+        {
+            try
+            {
+                // Debug.WriteLine($"{action.Method.Name}");
+                Tick += action;
+            }
+            catch (Exception ex)
+            {
+                Log.Error(ex.Message);
+            }
+        }
+
+        /// <summary>
+        /// Removes a tick function from the registry
+        /// </summary>
+        /// <param name="action"></param>
+        public void DeregisterTickHandler(Func<Task> action)
+        {
+            try
+            {
+                Tick -= action;
+            }
+            catch (Exception ex)
+            {
+                Log.Error(ex.Message);
+            }
+        }
+    }
+}
