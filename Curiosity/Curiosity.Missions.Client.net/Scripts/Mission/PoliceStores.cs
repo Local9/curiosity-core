b072a9c90dff63d3fbf6313e26bb227faa384ede
Antony Cook <kinglo0py@gmail.com>
2019-10-07 21:43:38 +0100

    added a store method

---
+++ Curiosity/Curiosity.Missions.Client.net/Scripts/Mission/PoliceStores.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CitizenFX.Core;
+
+namespace Curiosity.Missions.Client.net.Scripts.Mission
+{
+    class PoliceStores
+    {
+        static Dictionary<int, DataClasses.Mission.Store> stores = new Dictionary<int, DataClasses.Mission.Store>();
+
+        static public void Init()
+        {
+            List<DataClasses.Mission.MissionPedData> pedData = new List<DataClasses.Mission.MissionPedData>
+            {
+                new DataClasses.Mission.MissionPedData {  },
+            };
+            stores.Add(1, new DataClasses.Mission.Store { Name = "Store 1", Location = new Vector3(), missionPeds = pedData });
+        }
+
+    }
+}
