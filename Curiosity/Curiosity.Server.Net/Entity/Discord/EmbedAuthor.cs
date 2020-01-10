3c7bdcf0d8a2c82e41612e999cccbcd5fa48dbff
Antony Cook <kinglo0py@gmail.com>
2020-01-10 21:48:25 +0000

    added missing json properties

--- Curiosity/Curiosity.Server.Net/Entity/Discord/EmbedAuthor.cs
+++ Curiosity/Curiosity.Server.Net/Entity/Discord/EmbedAuthor.cs
@@ -1,8 +1,13 @@
-﻿namespace Curiosity.Server.net.Entity.Discord
+﻿using Newtonsoft.Json;
+
+namespace Curiosity.Server.net.Entity.Discord
 {
     public class EmbedAuthor
     {
+        [JsonProperty(PropertyName = "name", DefaultValueHandling = DefaultValueHandling.Ignore)]
         public string Name;
+
+        [JsonProperty(PropertyName = "icon_url", DefaultValueHandling = DefaultValueHandling.Ignore)]
         public string IconUrl;
     }
 }
