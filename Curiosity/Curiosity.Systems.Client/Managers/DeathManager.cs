c34caa9b0f6d874f0cdad1cee80d238f6e2fbe9c
Antony Cook <kinglo0py@gmail.com>
2020-03-01 11:36:34 +0000

    update death manager hospital positions

--- Curiosity/Curiosity.Systems.Client/Managers/DeathManager.cs
+++ Curiosity/Curiosity.Systems.Client/Managers/DeathManager.cs
@@ -5,6 +5,7 @@ using Curiosity.Systems.Client.Interface;
 using Curiosity.Systems.Library;
 using Curiosity.Systems.Library.Models;
 using System;
+using System.Collections.Generic;
 using System.Drawing;
 using System.Threading.Tasks;
 
@@ -14,6 +15,19 @@ namespace Curiosity.Systems.Client.Managers
     {
         public long Timestamp { get; set; }
         public bool WasDead { get; set; }
+
+        static List<Vector3> Hospitals = new List<Vector3>()
+        {
+            new Vector3(297.8683f, -584.3318f, 43.25863f),
+            new Vector3(356.434f, -598.5284f, 28.78098f),
+            new Vector3(307.5486f, -1434.502f, 29.86082f),
+            new Vector3(342.1533f, -1397.199f, 32.50924f),
+            new Vector3(-496.291f, -336.9345f, 34.50164f),
+            new Vector3(-449.0542f, -339.1804f, 34.50176f),
+            new Vector3(1827.909f, 3691.912f, 34.22427f),
+            new Vector3(-243.5568f, 6326.441f, 32.42619f),
+            new Vector3(4773.674f, -1935.709f, 17.06867f)
+        };
 
         [TickHandler(SessionWait = true)]
         private async Task OnTick()
@@ -42,13 +56,35 @@ namespace Curiosity.Systems.Client.Managers
                     API.ClearPedTasksImmediately(entity.Id);
                 }
 
+                int r = CuriosityPlugin.Rand.Next(Hospitals.Count);
+
+                Vector3 playerPos = Game.PlayerPed.Position;
+
+                Vector3 pos = new Vector3();
+
+                foreach (Vector3 hosPos in Hospitals)
+                {
+                    float distance = API.GetDistanceBetweenCoords(playerPos.X, playerPos.Y, playerPos.Z, hosPos.X, hosPos.Y, hosPos.Z, false);
+
+                    if (distance < 3000f)
+                    {
+                        pos = hosPos;
+                        break;
+                    }
+                }
+
+                if (pos.IsZero)
+                {
+                    pos = Hospitals[r];
+                }
+
                 if (Game.IsControlJustPressed(0, Control.Context))
                 {
                     API.DoScreenFadeOut(0);
 
                     // TODO: Charge cash
 
-                    player.Character.Revive(new Position(319.6946f, -573.5911f, 43.31747f, 231.9435f));
+                    player.Character.Revive(new Position(pos.X, pos.Y, pos.Z, Game.PlayerPed.Heading));
 
                     await BaseScript.Delay(3000);
 
@@ -58,7 +94,7 @@ namespace Curiosity.Systems.Client.Managers
                 {
                     API.DoScreenFadeOut(0);
 
-                    player.Character.Revive(new Position(319.6946f, -573.5911f, 43.31747f, 231.9435f));
+                    player.Character.Revive(new Position(pos.X, pos.Y, pos.Z, Game.PlayerPed.Heading));
 
                     await BaseScript.Delay(3000);
 
