f2550a73349e75dd0342a4cf71df201f5c0a3c6c
Antony Cook <kinglo0py@gmail.com>
2020-03-02 13:53:06 +0000

    added population manager

---
+++ Curiosity/Curiosity.Systems.Client/Managers/PopulationManager.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Curiosity.Systems.Client.Managers
+{
+    public class PopulationManipulation : Manager<PopulationManipulation>
+    {
+        public float Mulitplier { get; set; } = 1.0f;
+
+        [TickHandler]
+        private async Task OnTick()
+        {
+            API.SetVehicleDensityMultiplierThisFrame(Mulitplier);
+            API.SetPedDensityMultiplierThisFrame(Mulitplier);
+            API.SetRandomVehicleDensityMultiplierThisFrame(Mulitplier);
+            API.SetParkedVehicleDensityMultiplierThisFrame(Mulitplier);
+            API.SetScenarioPedDensityMultiplierThisFrame(Mulitplier, Mulitplier);
+
+            await Task.FromResult(0);
+        }
+    }
+}
