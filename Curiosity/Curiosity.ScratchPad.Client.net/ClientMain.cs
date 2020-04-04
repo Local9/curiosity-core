061c76e02df53457eb620cf07bd545d2d73ad86a
Antony Cook <kinglo0py@gmail.com>
2020-04-04 20:15:09 +0100

    Adding client scratch pad

---
+++ Curiosity/Curiosity.ScratchPad.Client.net/ClientMain.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Threading.Tasks;
+using CitizenFX.Core;
+using static CitizenFX.Core.Native.API;
+
+namespace Curiosity.ScratchPad.Client.net
+{
+    public class ClientMain : BaseScript
+    {
+
+    }
+}
