2be323f8c761077663af05565fbb966c4e6e1831
Antony Cook <kinglo0py@gmail.com>
2020-05-06 02:32:50 +0100

    Updated test code

--- Curiosity/Curiosity.Police.Client.net/Environment/WeaponScripts/SharkLauncher.cs
+++ Curiosity/Curiosity.Police.Client.net/Environment/WeaponScripts/SharkLauncher.cs
@@ -54,12 +54,45 @@ namespace Curiosity.Police.Client.net.Environment.WeaponScripts
                     string[] sharkarray = { "a_c_sharktiger", "a_c_sharkhammer" };
                     string deadshark = sharkarray[random.Next(sharkarray.Length + 1)];
                     Ped shark = await World.CreatePed(deadshark, spawnLocation);
-                    API.SetPedToRagdoll(shark.Handle, 0, 0, 0, true, true, true);
-                    shark.CanRagdoll = true;
-                    shark.Health = 0;
-
-                    shark.ApplyForce(Game.PlayerPed.ForwardVector * 500);
+                    SharkEntity sharkEntity = new SharkEntity(shark);
                 }
+            }
+        }
+
+        public class SharkEntity
+        {
+            public Ped Ped;
+            long TimeSpawned;
+
+            public SharkEntity(Ped ped)
+            {
+                Ped = ped;
+
+                API.SetPedToRagdoll(ped.Handle, 0, 0, 0, true, true, true);
+                ped.CanRagdoll = true;
+                ped.Health = 0;
+
+                ped.ApplyForce(Game.PlayerPed.ForwardVector * 500);
+
+                TimeSpawned = API.GetGameTimer();
+
+                Client.GetInstance().RegisterTickHandler(OnSharkSpawned);
+            }
+
+            async Task OnSharkSpawned()
+            {
+                while ((API.GetGameTimer() - TimeSpawned) < 60000)
+                {
+                    await Client.Delay(0);
+                }
+                if (Ped != null)
+                {
+                    if (Ped.Exists())
+                    {
+                        Ped.Delete();
+                    }
+                }
+                Client.GetInstance().DeregisterTickHandler(OnSharkSpawned);
             }
         }
     }
