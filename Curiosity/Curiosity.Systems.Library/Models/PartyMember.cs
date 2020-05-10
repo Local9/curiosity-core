f6fbde421f093e41544f0117870b89b3178deef1
Antony Cook <kinglo0py@gmail.com>
2020-05-10 16:49:28 +0100

    Added health and armor to party member

--- Curiosity/Curiosity.Systems.Library/Models/PartyMember.cs
+++ Curiosity/Curiosity.Systems.Library/Models/PartyMember.cs
@@ -12,5 +12,7 @@ namespace Curiosity.Systems.Library.Models
         public int Handle;
         public string Name;
         public bool Leader;
+        public int Health;
+        public int Armor;
     }
 }
