f019c0cf705e2aeb0fb7af65ade7ab9c4da46398
Antony Cook <kinglo0py@gmail.com>
2020-06-01 00:05:30 +0100

    Remove player information checks, newtonsoft being a fuck boy

--- Curiosity/Curiosity.Callouts.Client/Managers/PlayerManager.cs
+++ Curiosity/Curiosity.Callouts.Client/Managers/PlayerManager.cs
@@ -1,38 +1,24 @@
 ﻿using CitizenFX.Core;
+using CitizenFX.Core.UI;
+using Curiosity.Callouts.Client.Utils;
 using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Curiosity.Callouts.Client.Managers
 {
     class PlayerManager : BaseScript
     {
-        public static PlayerInformationModel playerInfo = new PlayerInformationModel();
+        static ExportDictionary exportDictionary;
 
         public PlayerManager()
         {
-            EventHandlers["curiosity:Client:Player:GetInformation"] += new Action<string>(OnPlayerInformation);
+
         }
 
-        private void OnPlayerInformation(string json)
+        public static bool IsDeveloper()
         {
-            playerInfo = JsonConvert.DeserializeObject<PlayerInformationModel>(json);
-        }
-
-        public class PlayerInformationModel
-        {
-            public string Handle;
-            public int UserId;
-            public int CharacterId;
-            public int RoleId;
-            public int Wallet;
-            public int BankAccount;
-            public Dictionary<string, object> Skills;
-
-            public bool IsDeveloper => RoleId == 4 || RoleId == 5;
+            return false; // NEED TO FIX NEWTONSOFT FUCK BOY ERRORS
         }
     }
 }
