796ab6828af5a6440251a9ebfdb2c9cd10568102
Antony Cook <kinglo0py@gmail.com>
2020-05-31 15:10:38 +0100

    Initial project layout for a new project

---
+++ Curiosity/Callout.Base/Callout.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalloutBase
+{
+    public abstract class Callout
+    {
+        public string Name;
+
+        public Callout()
+        {
+
+        }
+    }
+}
---
+++ Curiosity/Callouts/Callout.Test/Test.cs
@@ -0,0 +1,18 @@
+﻿using CitizenFX.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CalloutBase;
+
+namespace PoliceCallout.Test
+{
+    public class Test : Callout
+    {
+        public Test()
+        {
+            base.Name = "Test Callout";
+        }
+    }
+}
