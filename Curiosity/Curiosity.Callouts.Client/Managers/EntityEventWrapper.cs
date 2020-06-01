8b4ffc6e378310a736b6cc6e35fb008133ca5441
Antony Cook <kinglo0py@gmail.com>
2020-06-01 01:46:31 +0100

    Added entity event wrapper

---
+++ Curiosity/Curiosity.Callouts.Client/Managers/EntityEventWrapper.cs
@@ -0,0 +1,120 @@
+﻿using CitizenFX.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Curiosity.Callouts.Client.Managers
+{
+    class EntityEventWrapper
+    {
+        private PluginManager client = PluginManager.Instance;
+        private readonly static List<EntityEventWrapper> Wrappers;
+
+        private bool _isDead;
+
+        public Entity Entity
+        {
+            get;
+        }
+
+        public bool IsDead
+        {
+            get
+            {
+                return this.Entity.IsDead;
+            }
+            private set
+            {
+                if ((!value ? false : !this._isDead))
+                {
+                    EntityEventWrapper.OnDeathEvent onDeathEvent = this.Died;
+                    if (onDeathEvent != null)
+                    {
+                        onDeathEvent(this, this.Entity);
+                    }
+                }
+                this._isDead = value;
+            }
+        }
+
+        static EntityEventWrapper()
+        {
+            EntityEventWrapper.Wrappers = new List<EntityEventWrapper>();
+        }
+
+        public EntityEventWrapper(Entity ent)
+        {
+            this.Entity = ent;
+
+            client.RegisterTickHandler(this.EntityEventWrapperOnTick);
+            EntityEventWrapper.Wrappers.Add(this);
+        }
+
+        public void Abort()
+        {
+            EntityEventWrapper.OnWrapperAbortedEvent onWrapperAbortedEvent = this.Aborted;
+            if (onWrapperAbortedEvent != null)
+            {
+                onWrapperAbortedEvent(this, this.Entity);
+            }
+        }
+
+        public void Dispose()
+        {
+            client.DeregisterTickHandler(this.EntityEventWrapperOnTick);
+
+            EntityEventWrapper.Wrappers.Remove(this);
+            EntityEventWrapper.OnWrapperDisposedEvent onWrapperDisposedEvent = this.Disposed;
+            if (onWrapperDisposedEvent != null)
+            {
+                onWrapperDisposedEvent(this, this.Entity);
+            }
+        }
+
+        public static void Dispose(Entity entity)
+        {
+            EntityEventWrapper entityEventWrapper = EntityEventWrapper.Wrappers.Find((EntityEventWrapper w) => w.Entity == entity);
+            if (entityEventWrapper != null)
+            {
+                entityEventWrapper.Dispose();
+            }
+            EntityEventWrapper.Wrappers.Remove(entityEventWrapper);
+        }
+
+        public async Task EntityEventWrapperOnTick()
+        {
+            if ((this.Entity == null ? false : this.Entity.Exists()))
+            {
+                this.IsDead = this.Entity.IsDead;
+                EntityEventWrapper.OnWrapperUpdateEvent onWrapperUpdateEvent = this.Updated;
+                if (onWrapperUpdateEvent != null)
+                {
+                    onWrapperUpdateEvent(this, this.Entity);
+                }
+            }
+            else
+            {
+                this.Dispose();
+            }
+            await BaseScript.Delay(100);
+        }
+
+        public event EntityEventWrapper.OnWrapperAbortedEvent Aborted;
+
+        public event EntityEventWrapper.OnDeathEvent Died;
+
+        public event EntityEventWrapper.OnWrapperDisposedEvent Disposed;
+
+        public event EntityEventWrapper.OnWrapperUpdateEvent Updated;
+
+        public delegate void OnDeathEvent(EntityEventWrapper sender, Entity entity);
+
+        public delegate void OnWrapperAbortedEvent(EntityEventWrapper sender, Entity entity);
+
+        public delegate void OnWrapperDisposedEvent(EntityEventWrapper sender, Entity entity);
+
+        public delegate void OnWrapperUpdateEvent(EntityEventWrapper sender, Entity entity);
+    }
+}
