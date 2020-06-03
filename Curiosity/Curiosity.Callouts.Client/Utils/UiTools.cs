de1ec2635140d6081bc791e155caa65bd5aae86d
Antony Cook <kinglo0py@gmail.com>
2020-06-03 02:33:37 +0100

    Added own tool for notifications

---
+++ Curiosity/Curiosity.Callouts.Client/Utils/UiTools.cs
@@ -0,0 +1,21 @@
+﻿using CitizenFX.Core.Native;
+
+namespace Curiosity.Callouts.Client.Utils
+{
+    class UiTools
+    {
+        static public void Dispatch(string subject, string message)
+        {
+            Advanced("CHAR_911", 2, "Dispatch", subject, message, 2);
+        }
+
+        static public void Advanced(string icon, int iconType, string title, string subject, string message, int gtaColorId)
+        {
+            API.SetNotificationBackgroundColor(gtaColorId);
+            API.SetNotificationTextEntry("STRING");
+            API.AddTextComponentString(message);
+            API.SetNotificationMessage(icon, icon, false, iconType, title, subject);
+            API.DrawNotification(false, true);
+        }
+    }
+}
