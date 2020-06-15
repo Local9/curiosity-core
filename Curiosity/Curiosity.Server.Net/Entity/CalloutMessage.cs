8465349348ffb8a8e8e1e7f19609f0a032e9f85f
Antony Cook <kinglo0py@gmail.com>
2020-06-15 21:42:48 +0100

    Added number of arrests

--- Curiosity/Curiosity.Server.Net/Entity/CalloutMessage.cs
+++ Curiosity/Curiosity.Server.Net/Entity/CalloutMessage.cs
@@ -2,47 +2,49 @@
 
 namespace Curiosity.Server.net.Entity
 {
-    class CalloutMessage
+    public class CalloutMessage
     {
-        public string Name;
-        public bool Success;
-        public bool IsCalloutFinished;
-        public int NumberRescued;
-        public CalloutType CalloutType;
+        public string Name { get; set; }
+        public bool Success { get; set; }
+        public bool IsCalloutFinished { get; set; }
+        public int NumberRescued { get; set; }
+        public CalloutType CalloutType { get; set; }
         public List<CalloutPed> calloutPed = new List<CalloutPed>();
         public List<CalloutVehicle> calloutVehicle = new List<CalloutVehicle>();
+        public int NumberArrested { get; set; }
     }
 
     public class CalloutVehicle
     {
-        public bool IsDestroyed;
-        public bool IsImpounded;
-        public bool IsStolen;
+        public bool IsDestroyed { get; set; }
+        public bool IsImpounded { get; set; }
+        public bool IsStolen { get; set; }
 
         public List<CalloutItem> Items = new List<CalloutItem>();
     }
 
     public class CalloutPed
     {
-        public string Name;
+        public string Name { get; set; }
 
-        public bool IsAlive;
-        public bool IsDrunk;
-        public bool IsUnderInfluence;
+        public bool IsAlive { get; set; }
+        public bool IsDrunk { get; set; }
+        public bool IsUnderInfluence { get; set; }
 
         public List<CalloutItem> Items = new List<CalloutItem>();
     }
 
     public class CalloutItem
     {
-        public bool IsIllegal;
-        public string Label;
+        public bool IsIllegal { get; set; }
+        public string Label { get; set; }
     }
 
     public enum CalloutType
     {
         HOSTAGE_RESCUE,
         STOLEN_VEHICLE,
-        PARKING_VIOLATION
+        PARKING_VIOLATION,
+        CYCLIST_ON_FREEWAY
     }
 }
