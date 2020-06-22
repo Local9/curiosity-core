a323142fa14dc944f66e08427323a60f114fcf31
Antony Cook <kinglo0py@gmail.com>
2020-06-22 22:58:21 +0100

    Adding prisoner transport

---
+++ Curiosity/Curiosity.Callouts.Client/Managers/PrisonerTransportManager.cs
@@ -0,0 +1,22 @@
+﻿using CitizenFX.Core;
+using CitizenFX.Core.Native;
+using CitizenFX.Core.UI;
+using Curiosity.Callouts.Client.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Curiosity.Callouts.Client.Utils.Collections;
+using Ped = Curiosity.Callouts.Client.Classes.Ped;
+using Vehicle = Curiosity.Callouts.Client.Classes.Vehicle;
+
+namespace Curiosity.Callouts.Client.Managers
+{
+    internal class PrisonerTransportManager : BaseScript
+    {
+        public static bool IsActive = false;
+        static List<Ped> Prisoners;
+        static int managerProgress = 0;
+    }
+}
