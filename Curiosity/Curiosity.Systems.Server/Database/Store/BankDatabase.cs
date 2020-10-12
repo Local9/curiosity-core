d12bf51a09e35dcf69858c0ae34283e86eea276d
Antony Cook <kinglo0py@gmail.com>
2020-10-12 02:03:29 +0100

    notes

---
+++ Curiosity/Curiosity.Systems.Server/Database/Store/BankDatabase.cs
@@ -0,0 +1,28 @@
+﻿using CitizenFX.Core.Native;
+using Curiosity.Systems.Library.Models;
+using Curiosity.Systems.Server.Extensions;
+using GHMatti.Data.MySQL.Core;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Curiosity.Systems.Server.Database.Store
+{
+    class BankDatabase
+    {
+        // process
+        // buy item -> tell server item user wants to buy
+        // server adds item to players inventory/account depending on type
+        // on completion, call back to the client and inform purchase
+
+        public static async Task DecreaseCash(ulong discordId, int amount)
+        {
+
+        }
+
+        public static async Task IncreaseCash(ulong discordId, int amount)
+        {
+
+        }
+    }
+}
