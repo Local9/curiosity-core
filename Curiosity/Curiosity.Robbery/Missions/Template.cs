dd38472da366f1ce729d87897a3b6c45b383cde9
Antony Cook <kinglo0py@gmail.com>
2020-11-01 23:56:27 +0000

    Template test worked successfully

---
+++ Curiosity/Curiosity.Robbery/Missions/Template.cs
@@ -0,0 +1,34 @@
+﻿using CitizenFX.Core;
+using CitizenFX.Core.UI;
+using Curiosity.MissionManager.Client;
+using Curiosity.MissionManager.Client.Attributes;
+using Curiosity.MissionManager.Client.Utils;
+using Curiosity.MissionManager.Shared.Utils;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Vehicle = Curiosity.MissionManager.Client.Classes.Vehicle;
+using Ped = Curiosity.MissionManager.Client.Classes.Ped;
+
+namespace Curiosity.StolenVehicle.Missions
+{
+    [MissionInfo("Template", "Template", 0f, 0f, 0f, MissionType.Mission, true, "None")]
+    public class Template : Mission
+    {
+        public async override void Start()
+        {
+
+            MissionManager.Instance.RegisterTickHandler(OnMissionTick);
+        }
+
+        public override void End()
+        {
+            MissionManager.Instance.DeregisterTickHandler(OnMissionTick);
+        }
+
+        async Task OnMissionTick()
+        {
+
+        }
+    }
+}
