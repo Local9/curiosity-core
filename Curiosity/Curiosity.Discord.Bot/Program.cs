9068f977661536132d8bc30434922733a0ee204f
Antony Cook <kinglo0py@gmail.com>
2020-11-07 11:09:27 +0000

    removed start up message

--- Curiosity/Curiosity.Discord.Bot/Program.cs
+++ Curiosity/Curiosity.Discord.Bot/Program.cs
@@ -75,8 +75,6 @@ namespace Curiosity.LifeV.Bot
                 _timedEvents = new TimedEvents(_client, guildId);
             }
 
-            _client.GetGuild(guildId).GetTextChannel(773248492939247626).SendMessageAsync($"Bot Started - System Time: {DateTime.Now}"); // MOVE TO CONFIG
-
             await Task.Delay(-1);
         }
 
