3717bb48259798127658b442a1abaed8023ee5ee
Antony Cook <kinglo0py@gmail.com>
2020-11-08 19:03:21 +0000

    added a discord export

---
+++ Curiosity/Curiosity.MissionManager.Client/Managers/ExportsManager.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Curiosity.MissionManager.Client.Managers
+{
+    public class ExportsManager : Manager<ExportsManager>
+    {
+        public override async void Begin()
+        {
+            Instance.ExportRegistry.Add("Discord", new Func<string, string, string, bool>(
+                (smallAsset, smallAssetTest, status) =>
+                {
+                    if (!string.IsNullOrEmpty(smallAsset))
+                        Instance.DiscordRichPresence.SmallAsset = smallAsset;
+
+                    if (!string.IsNullOrEmpty(smallAssetTest))
+                        Instance.DiscordRichPresence.SmallAssetText = smallAssetTest;
+
+                    if (!string.IsNullOrEmpty(status))
+                        Instance.DiscordRichPresence.Status = status;
+
+                    if (!string.IsNullOrEmpty(status) || !string.IsNullOrEmpty(smallAssetTest) || !string.IsNullOrEmpty(smallAsset))
+                    {
+                        Instance.DiscordRichPresence.Commit();
+                        return true;
+                    }
+
+                    return false;
+                }
+            ));
+        }
+    }
+}
