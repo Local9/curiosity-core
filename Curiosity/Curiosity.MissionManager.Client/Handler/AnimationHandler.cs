bb7a35736022a48f301429ecdb51861815d54f5f
Antony Cook <kinglo0py@gmail.com>
2020-11-16 23:46:03 +0000

    add handler to check if animation has been triggered

--- Curiosity/Curiosity.MissionManager.Client/Handler/AnimationHandler.cs
+++ Curiosity/Curiosity.MissionManager.Client/Handler/AnimationHandler.cs
@@ -6,24 +6,38 @@ namespace Curiosity.MissionManager.Client.Handler
 {
     public class AnimationHandler
     {
+        static bool animationPlaying = false;
+
         public static async Task AnimationSearch()
         {
+            if (animationPlaying) return;
+            animationPlaying = true;
+
             AnimationBuilder animationBuilder1 = new AnimationBuilder().Select("PROP_HUMAN_BUM_BIN");
             Cache.Entity.AnimationQueue.PlayDirectInQueue(animationBuilder1);
             await BaseScript.Delay(3000);
             Cache.Entity.Task.ClearAll();
+
+            animationPlaying = false;
         }
 
         public static async Task AnimationClipboard()
         {
+            if (animationPlaying) return;
+            animationPlaying = true;
+
             AnimationBuilder animationBuilder1 = new AnimationBuilder().Select("WORLD_HUMAN_CLIPBOARD");
             Cache.Entity.AnimationQueue.PlayDirectInQueue(animationBuilder1);
             await BaseScript.Delay(3000);
             Cache.Entity.Task.ClearAll();
+            animationPlaying = false;
         }
 
         public static void AnimationRadio()
         {
+            if (animationPlaying) return;
+            animationPlaying = true;
+
             AnimationBuilder animationBuilder1 = new AnimationBuilder().Select("random@arrests", "generic_radio_chatter");
             AnimationBuilder animationBuilder2 = new AnimationBuilder().Select("random@arrests", "generic_radio_emter");
             AnimationBuilder animationBuilder3 = new AnimationBuilder().Select("random@arrests", "generic_radio_exit");
@@ -33,6 +47,8 @@ namespace Curiosity.MissionManager.Client.Handler
             Cache.Entity.AnimationQueue.AddToQueue(animationBuilder3);
 
             Cache.Entity.AnimationQueue.PlayQueue();
+
+            animationPlaying = false;
         }
     }
 }
