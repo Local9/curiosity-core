4c0e3e2b2103984cfd1c16e380d975e42ab82dec
Antony Cook <kinglo0py@gmail.com>
2020-11-19 15:24:26 +0000

    Added StringValueAttribute

--- Curiosity/Curiosity.MissionManager.Shared/Enums/PatrolZone.cs
+++ Curiosity/Curiosity.MissionManager.Shared/Enums/PatrolZone.cs
@@ -1,12 +1,20 @@
-﻿namespace Curiosity.Systems.Library.Enums
+﻿using Curiosity.Systems.Library.Attributes;
+
+namespace Curiosity.Systems.Library.Enums
 {
     public enum PatrolZone
     {
+        [StringValue("Anywhere")]
         Anywhere,
+        [StringValue("City")]
         City,
+        [StringValue("County")]
         County,
+        [StringValue("Ocean")]
         Ocean,
+        [StringValue("Highway")]
         Highway,
+        [StringValue("Rural")]
         Rural
     }
 }
