31a1ee5ad8088128bd0036299168594601138d54
Antony Cook <kinglo0py@gmail.com>
2020-11-19 23:04:39 +0000

    Added events for Patrol and Duty management

--- Curiosity/Curiosity.MissionManager.Client/Managers/JobManager.cs
+++ Curiosity/Curiosity.MissionManager.Client/Managers/JobManager.cs
@@ -1,21 +1,20 @@
 ﻿using CitizenFX.Core;
 using CitizenFX.Core.Native;
 using CitizenFX.Core.UI;
+using Curiosity.MissionManager.Client.Diagnostics;
 using Curiosity.MissionManager.Client.Handler;
 using Curiosity.MissionManager.Client.Utils;
 using Curiosity.Systems.Library.Enums;
 using Curiosity.Systems.Library.Events;
 using Curiosity.Systems.Library.EventWrapperLegacy;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace Curiosity.MissionManager.Client.Managers
 {
     public class JobManager : Manager<JobManager>
     {
+        private const string JOB_POLICE = "police";
         internal static PatrolZone PatrolZone;
         internal static bool IsOnDuty;
         internal static bool IsOfficer;
@@ -25,39 +24,55 @@ namespace Curiosity.MissionManager.Client.Managers
 
         public override void Begin()
         {
+            Logger.Info($"------------- [JobManager] Begin -----------------");
+
+            Instance.EventRegistry[LegacyEvents.Client.PoliceDutyEvent] += new Action<bool, bool, string>(OnJobDutyEvent);
+            Instance.EventRegistry[LegacyEvents.Client.PolicePatrolZone] += new Action<int>(OnJobPatrolZone);
+
             EventSystem.Attach(LegacyEvents.Client.PolicePatrolZone, new EventCallback(metadata =>
             {
                 PatrolZone = (PatrolZone)metadata.Find<int>(0);
                 return null;
             }));
+        }
 
-            EventSystem.Attach(LegacyEvents.Client.PoliceDutyEvent, new AsyncEventCallback(async metadata =>
+        private void OnJobPatrolZone(int patrolZone)
+        {
+            Logger.Debug($"OnJobPatrolZone: {patrolZone}");
+
+            PatrolZone = (PatrolZone)patrolZone;
+        }
+
+        private async void OnJobDutyEvent(bool active, bool onDuty, string job)
+        {
+            Logger.Debug($"OnJobDutyEvent: {job}:{onDuty}");
+
+            IsOnDuty = onDuty;
+
+            if (IsOfficer != (job == JOB_POLICE))
+                HasShownScaleform = false;
+
+            IsOfficer = (job == JOB_POLICE);
+
+            if (IsOfficer)
             {
-                IsOnDuty = metadata.Find<bool>(1);
-                IsOfficer = (metadata.Find<string>(2) == "police");
+                if (!HasShownScaleform)
+                    ShowScaleformRules();
 
-                if (IsOfficer)
-                {
-                    if (HasShownScaleform)
-                        ShowScaleformRules();
+                Game.PlayerPed.RelationshipGroup = (uint)Collections.RelationshipHash.Cop;
 
-                    Game.PlayerPed.RelationshipGroup = (uint)Collections.RelationshipHash.Cop;
+                MarkerHandler.Init();
+                await BaseScript.Delay(100);
+                MarkerArrest.Init();
+            }
+            else
+            {
+                Game.PlayerPed.RelationshipGroup = (uint)Collections.RelationshipHash.Player;
 
-                    MarkerHandler.Init();
-                    await BaseScript.Delay(100);
-                    MarkerArrest.Init();
-                }
-                else
-                {
-                    Game.PlayerPed.RelationshipGroup = (uint)Collections.RelationshipHash.Player;
-
-                    MarkerHandler.Dispose();
-                    await BaseScript.Delay(100);
-                    MarkerArrest.Dispose();
-                }
-
-                return null;
-            }));
+                MarkerHandler.Dispose();
+                await BaseScript.Delay(100);
+                MarkerArrest.Dispose();
+            }
         }
 
         static async void ShowScaleformRules()
@@ -91,13 +106,13 @@ namespace Curiosity.MissionManager.Client.Managers
                 await BaseScript.Delay(0);
             }
 
-            string description = "~w~Guides can be found via the Intraction Menu [M].~w~\n";
+            string description = "~w~Guides can be found via the Intraction Menu [~o~M~w~].\n";
             description += "~r~~h~DO NOT~h~~w~ Pull over other players.\n";
             description += "~r~~h~DO NOT~h~~w~ Drive recklessly.\n";
             description += "~r~~h~DO NOT~h~~w~ Force people to RolePlay, doing so will get you kicked.\n";
             description += "\n";
             description += "~g~To Get Started:\n";
-            description += "~w~Open the Activity Menu [F1]\n";
+            description += "~w~Open the Activity Menu [~o~F1~w~]\n";
             description += "~w~Here you will find all your options to your job.\n";
             description += "Finally, keep it friendly and we'll all get along.\n";
             description += "~r~∑~b~∑~g~∑~y~∑~p~∑~o~∑";
@@ -108,11 +123,11 @@ namespace Curiosity.MissionManager.Client.Managers
             description += "~r~∑~b~∑~g~∑~y~∑~p~∑~o~∑";
             description += "~r~∑~b~∑~g~∑~y~∑~p~∑~o~∑";
             description += "~r~∑\n";
-            description += "~o~REMEMBER Press ~b~M~o~ to read the guides for more information!~w~\n";
+            description += "~o~REMEMBER Press ~b~~h~M~h~~o~ to read the guides for more information!~w~\n";
             description += "~b~Forums~w~: forums.lifev.net / ~b~Discord~w~: discord.lifev.net";
 
             scaleform.CallFunction("SETUP_TABS", 1, false);
-            const string dictTexture = "online_policies";
+            const string dictTexture = "www_arenawar_tv";
             await LoadDict(dictTexture);
             scaleform.CallFunction("SETUP_TABS", true);
             scaleform.CallFunction("SET_BIGFEED_INFO", "Hello", description, 0, dictTexture, "bg_top_left", $"~y~Police Duty", "deprecated", $"Police Duty in Life V!", 0);
