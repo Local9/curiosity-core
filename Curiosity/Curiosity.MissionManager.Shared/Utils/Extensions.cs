849901db63d8ccc4de71a1dbcdc534dec5e71818
Antony Cook <kinglo0py@gmail.com>
2020-11-19 15:31:57 +0000

    Move extension to correct location

--- Curiosity/Curiosity.MissionManager.Shared/Utils/Extensions.cs
+++ Curiosity/Curiosity.MissionManager.Shared/Utils/Extensions.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using Curiosity.Systems.Library.Attributes;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 
 namespace Curiosity.Systems.Library.Utils
 {
@@ -57,6 +59,29 @@ namespace Curiosity.Systems.Library.Utils
                 yield return values.Last.Value;
                 values.RemoveLast();
             }
+        }
+        
+        /// <summary>
+         /// Will get the string value for a given enums value, this will
+         /// only work if you assign the StringValue attribute to
+         /// the items in your enum.
+         /// </summary>
+         /// <param name="value"></param>
+         /// <returns></returns>
+        public static string GetStringValue(this Enum value)
+        {
+            // Get the type
+            Type type = value.GetType();
+
+            // Get fieldinfo for this type
+            FieldInfo fieldInfo = type.GetField(value.ToString());
+
+            // Get the stringvalue attributes
+            StringValueAttribute[] attribs = fieldInfo.GetCustomAttributes(
+                typeof(StringValueAttribute), false) as StringValueAttribute[];
+
+            // Return the first if there was a match.
+            return attribs.Length > 0 ? attribs[0].StringValue : null;
         }
     }
 }
