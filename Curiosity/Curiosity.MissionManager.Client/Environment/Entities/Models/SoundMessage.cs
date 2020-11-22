3c0d2f3fe581bff5ba3607f9128d559b9a23513f
Antony Cook <kinglo0py@gmail.com>
2020-11-22 14:21:37 +0000

    Added Howler JS for audio files

---
+++ Curiosity/Curiosity.MissionManager.Client/Environment/Entities/Models/SoundMessage.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+
+namespace Curiosity.MissionManager.Client.Environment.Entities.Models
+{
+    class SoundMessage
+    {
+        public string transactionType;
+        public string transactionFile;
+        public float transactionVolume;
+        public int delayAudio;
+
+        public List<string> audioQueue = new List<string>();
+
+        public SoundMessage(float vol = 0.3f)
+        {
+            transactionType = "playSound";
+            transactionVolume = vol;
+        }
+
+        public SoundMessage(string file, float vol = 0.3f, int delay = 1000)
+        {
+            transactionFile = file;
+            transactionType = "playSound";
+            transactionVolume = vol;
+            delayAudio = delay;
+        }
+
+        public SoundMessage(string type, string file, float vol = 0.3f)
+        {
+            transactionFile = file;
+            transactionType = type;
+            transactionVolume = vol;
+        }
+    }
+}
---
+++ Curiosity/Curiosity.MissionManager.Client/Handler/HowlerHandler.cs
@@ -0,0 +1,63 @@
+﻿using CitizenFX.Core.Native;
+using Curiosity.MissionManager.Client.Environment.Entities.Models;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace Curiosity.MissionManager.Client.Handler
+{
+    class HowlerHandler
+    {
+        public static float AudioVolume = 0.3f;
+
+        public static void PlayAudio(List<string> audioFiles)
+        {
+            SoundMessage soundMessage = new SoundMessage(AudioVolume);
+
+            audioFiles.ForEach(file =>
+            {
+                string fileLoc = $"./audio/{file}.wav";
+                soundMessage.audioQueue.Add(fileLoc);
+            });
+
+            string message = JsonConvert.SerializeObject(soundMessage);
+            API.SendNuiMessage(message);
+        }
+
+        public static void PlaySFX(List<string> audioFiles)
+        {
+            SoundMessage soundMessage = new SoundMessage(AudioVolume);
+
+            audioFiles.ForEach(file =>
+            {
+                string fileLoc = $"./sfx/{file}.wav";
+                soundMessage.audioQueue.Add(fileLoc);
+            });
+
+            string message = JsonConvert.SerializeObject(soundMessage);
+            API.SendNuiMessage(message);
+        }
+
+        public static void PlaySFX(List<string> audioFiles, float audioVolume)
+        {
+            if (audioVolume > AudioVolume)
+                audioVolume = AudioVolume;
+
+            SoundMessage soundMessage = new SoundMessage(audioVolume);
+
+            audioFiles.ForEach(file =>
+            {
+                string fileLoc = $"./sfx/{file}.wav";
+                soundMessage.audioQueue.Add(fileLoc);
+            });
+
+            string message = JsonConvert.SerializeObject(soundMessage);
+            API.SendNuiMessage(message);
+        }
+
+        public static void PlayAudioFile(string audioFile)
+        {
+            SoundMessage soundMessage = new SoundMessage(audioFile, AudioVolume);
+            API.SendNuiMessage(JsonConvert.SerializeObject(soundMessage));
+        }
+    }
+}
