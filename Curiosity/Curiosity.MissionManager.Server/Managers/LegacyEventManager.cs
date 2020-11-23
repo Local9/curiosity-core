f886f3905869f5c3d0c0d4a350c75c6a945a764d
Antony Cook <kinglo0py@gmail.com>
2020-11-23 21:49:29 +0000

    Added legacy event manager

---
+++ Curiosity/Curiosity.MissionManager.Server/Managers/LegacyEventManager.cs
@@ -0,0 +1,22 @@
+﻿using CitizenFX.Core;
+using Curiosity.MissionManager.Server.Events;
+using Curiosity.Systems.Library.Events;
+
+namespace Curiosity.MissionManager.Server.Managers
+{
+    public class LegacyEventManager : Manager<LegacyEventManager>
+    {
+        public override void Begin()
+        {
+            EventSystem.GetModule().Attach("vehicle:delete", new AsyncEventCallback(async metadata =>
+            {
+                int senderHandle = metadata.Sender;
+                int networkId = metadata.Find<int>(0);
+
+                BaseScript.TriggerClientEvent("curiosity:Player:Vehicle:Delete:NetworkId", networkId);
+
+                return true;
+            }));
+        }
+    }
+}
