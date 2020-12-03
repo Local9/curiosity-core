404eb232a327057fd6ee0b4c48f83ce24bf2ec64
Antony Cook <kinglo0py@gmail.com>
2020-12-03 11:47:11 +0000

    Added method to get total required experience for a level

--- Curiosity/Curiosity.MissionManager.Server/Managers/LevelingManager.cs
+++ Curiosity/Curiosity.MissionManager.Server/Managers/LevelingManager.cs
@@ -17,5 +17,18 @@ namespace Curiosity.MissionManager.Server.Managers
 			int firstPass = (int)Math.Floor(level + (300.0f * Math.Pow(2.0f, level / 7.0f)));
 			return firstPass / 4;
 		}
+
+		static int TotalExperienceRequiredForALevel(int level)
+        {
+			int firstPass = 0;
+			int secondPass = 0;
+
+			for(int levelCycle = 1; levelCycle < level; levelCycle++)
+            {
+				firstPass += (int)Math.Floor(levelCycle + (300.0f * Math.Pow(2.0f, levelCycle / 7.0f)));
+				secondPass = firstPass / 4;
+			}
+			return secondPass;
+        }
 	}
 }
