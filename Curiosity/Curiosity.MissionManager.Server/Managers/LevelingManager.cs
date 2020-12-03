fdd9a951099e037e8a4a32c89da1fa62ececd89b
Antony Cook <kinglo0py@gmail.com>
2020-12-03 11:42:11 +0000

    Initial leveling maths

---
+++ Curiosity/Curiosity.MissionManager.Server/Managers/LevelingManager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Curiosity.MissionManager.Server.Managers
+{
+    class LevelingManager : Manager<LevelingManager>
+    {
+		// get level
+
+		// check required
+		static int ExperienceToNextLevel(int level)
+		{
+			int levelMinus = level - 1;
+			int firstPass = (int)Math.Floor(level + (300.0f * Math.Pow(2.0f, level / 7.0f)));
+			return firstPass / 4;
+		}
+	}
+}
