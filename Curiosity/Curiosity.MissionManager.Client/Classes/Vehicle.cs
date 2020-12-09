503656d79cfc23444f34ed5a19bfcc177d7b76d2
Antony Cook <kinglo0py@gmail.com>
2020-12-09 15:15:07 +0000

    Fix error in logic

--- Curiosity/Curiosity.MissionManager.Client/Classes/Vehicle.cs
+++ Curiosity/Curiosity.MissionManager.Client/Classes/Vehicle.cs
@@ -296,7 +296,7 @@ namespace Curiosity.MissionManager.Client.Classes
                 }
             }
 
-            if (Game.PlayerPed.CurrentVehicle == PlayerManager.PersonalVehicle && Game.PlayerPed.IsInVehicle() Fx.Driver.Exists())
+            if (Game.PlayerPed.CurrentVehicle == PlayerManager.PersonalVehicle && Game.PlayerPed.IsInVehicle() && Fx.Driver.Exists())
             {
                 if (Game.PlayerPed.CurrentVehicle.ClassType == VehicleClass.Emergency
                 && !Mission.isOnMission && !IsIgnored)
