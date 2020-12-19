f337c3ba354ec722710eff83ffc3a5f698ab6dad
Antony Cook <kinglo0py@gmail.com>
2020-12-19 19:23:44 +0000

    version

--- Curiosity/Curiosity.MissionManager.Server/PluginManager.cs
+++ Curiosity/Curiosity.MissionManager.Server/PluginManager.cs
@@ -51,7 +51,7 @@ namespace Curiosity.MissionManager.Server
         {
             try
             {
-                API.SetConvarServerInfo("Curiosity Missions", "v1.0.0.2490");
+                API.SetConvarServerInfo("Curiosity Missions", "v1.0.0.2493");
 
                 IsDebugging = API.GetConvar("diagnostics_debug", "false") == "true";
 
