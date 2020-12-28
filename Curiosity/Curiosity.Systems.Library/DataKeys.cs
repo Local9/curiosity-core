762e124ee420afe60201656a3dddcab55e8fff93
Antony Cook <kinglo0py@gmail.com>
2020-12-28 20:18:31 +0000

    Added central library for events

---
+++ Curiosity/Curiosity.Systems.Library/DataKeys.cs
@@ -0,0 +1,8 @@
+﻿namespace Curiosity.Systems.Library
+{
+    public class DataKeys
+    {
+        public const string SERVER_EVENT_KEY = "25cQDd5Mihk1hqLi9UDQbEP53";
+        public const string MISSION_EVENT_KEY = "z5Id5C72n3EILEH5W283837uy";
+    }
+}
