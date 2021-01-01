c6d2a02ba964803d880b38c2a775058c4d82f895
Antony Cook <kinglo0py@gmail.com>
2021-01-01 23:11:14 +0000

    update styles

--- Curiosity/Curiosity.Core.Server/Managers/MarqueeManager.cs
+++ Curiosity/Curiosity.Core.Server/Managers/MarqueeManager.cs
@@ -15,14 +15,14 @@ namespace Curiosity.Core.Server.Managers
 
         List<string> MarqueeMessages = new List<string>()
         {
-            "<span style='color:dodgerblue'>Forums:</span> <span style='color:ff9900'>forums.lifev.net</span>",
-            "<span style='color:dodgerblue'>Discord:</span> <span style='color:#ff9900'>discord.lifev.net</span>",
-            "Find any bugs? Please report them to our forums: <span style='color:#ff9900'>forums.lifev.net</span>",
+            "<span style='color:dodgerblue; font-weight: bold'>Forums:</span> <span style='color:#ff9900; font-weight: bold'>forums.lifev.net</span>",
+            "<span style='color:dodgerblue; font-weight: bold'>Discord:</span> <span style='color:#ff9900; font-weight: bold'>discord.lifev.net</span>",
+            "Find any bugs? Please report them to our forums: <span style='color:#ff9900; font-weight: bold'>forums.lifev.net</span>",
             "Please use reports to notify us of rule breakers",
             "Please remember to be friendly to one another",
-            "To read our rules Press the <span style='color:dodgerblue'>F11</span> or <span style='color:dodgerblue'>HOME</span> key",
-            "<span style='color:dodgerblue'>Support us:</span> <Span color='#ff9900'>patreon.com/lifev</span>",
-            "To read our guides press the <span style='color:dodgerblue'>F11</span> or <span style='color:dodgerblue'>HOME</span> key",
+            "To read our rules Press the <span style='color:dodgerblue; font-weight: bold'>F11</span> or <span style='color:dodgerblue; font-weight: bold'>HOME</span> key",
+            "<span style='color:dodgerblue; font-weight: bold'>Support us:</span> <Span color='#ff9900; font-weight: bold'>patreon.com/lifev</span>",
+            "To read our guides press the <span style='color:dodgerblue; font-weight: bold'>F11</span> or <span style='color:dodgerblue; font-weight: bold'>HOME</span> key",
         };
 
         public override void Begin()
