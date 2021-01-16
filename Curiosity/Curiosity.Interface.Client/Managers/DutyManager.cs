24a1de1f40e99a47684c3c52f962e8fc2a3f7697
Antony Cook <kinglo0py@gmail.com>
2021-01-16 14:47:27 +0000

    conform code

--- Curiosity/Curiosity.Interface.Client/Managers/DutyManager.cs
+++ Curiosity/Curiosity.Interface.Client/Managers/DutyManager.cs
@@ -13,15 +13,16 @@ namespace Curiosity.Interface.Client.Managers
             Instance.EventRegistry[LegacyEvents.Client.PoliceDutyEvent] += new Action<bool, bool, string>(OnDutyState);
         }
 
-        private static void OnDutyState(bool active, bool onduty, string job)
+        private static void OnDutyState(bool active, bool onDuty, string job)
         {
-            string jsn = new JsonBuilder().Add("operation", "JOB_ACTIVITY")
-                    .Add("jobActive", active)
-                    .Add("jobOnDuty", onduty)
-                    .Add("jobTitle", job.ToTitleCase())
-                    .Build();
+            string msg = new JsonBuilder()
+                .Add("operation", "JOB_ACTIVITY")
+                .Add("jobActive", active)
+                .Add("jobOnDuty", onDuty)
+                .Add("jobTitle", job.ToTitleCase())
+                .Build();
 
-            API.SendNuiMessage(jsn);
+            API.SendNuiMessage(msg);
         }
     }
 }
