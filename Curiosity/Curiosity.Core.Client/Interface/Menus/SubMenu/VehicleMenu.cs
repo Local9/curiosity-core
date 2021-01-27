d002822329c496e4ed107eef717162b6ca0af8b5
Antony Cook <kinglo0py@gmail.com>
2021-01-27 21:59:20 +0000

    Initial vehicle menu

---
+++ Curiosity/Curiosity.Core.Client/Interface/Menus/SubMenu/VehicleMenu.cs
@@ -0,0 +1,26 @@
+﻿using NativeUI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Curiosity.Core.Client.Interface.Menus.SubMenu
+{
+    class VehicleMenu
+    {
+        public UIMenu CreateMenu(UIMenu menu)
+        {
+            menu.OnMenuOpen += Menu_OnMenuOpen;
+
+            return menu;
+        }
+
+        private void Menu_OnMenuOpen(UIMenu menu)
+        {
+            menu.Clear();
+
+
+        }
+    }
+}
