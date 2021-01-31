5dfe8d489cbf47c9fb443481cd1eda2a0da35bf5
Antony Cook <kinglo0py@gmail.com>
2021-01-31 01:04:26 +0000

    ped headshot

---
+++ Curiosity/Curiosity.Core.Client/Extensions/PedExtensions.cs
@@ -0,0 +1,21 @@
+﻿using CitizenFX.Core;
+using CitizenFX.Core.Native;
+using System.Threading.Tasks;
+
+namespace Curiosity.Core.Client.Extensions
+{
+    public static class PedExtensions
+    {
+        public static async Task<string> GetHeadshot(this Ped ped)
+        {
+            int headshot = API.RegisterPedheadshot(ped.Handle);
+
+            while (!API.IsPedheadshotReady(headshot))
+            {
+                await BaseScript.Delay(0);
+            }
+
+            return API.GetPedheadshotTxdString(headshot);
+        }
+    }
+}
