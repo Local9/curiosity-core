b8958bed9c410b2842fed5e56fe989540d4c4cf0
Antony Cook <kinglo0py@gmail.com>
2021-02-01 13:50:03 +0000

    Revert some passive methods for testing

--- Curiosity/Curiosity.Core.Client/Interface/Menus/InteractionMenu.cs
+++ Curiosity/Curiosity.Core.Client/Interface/Menus/InteractionMenu.cs
@@ -22,6 +22,7 @@ namespace Curiosity.Core.Client.Interface.Menus
         private int gpsIndex = 0;
         private UIMenuListItem mlGpsLocations = new UIMenuListItem("GPS", gpsLocations, 0);
 
+        // private UIMenuItem miPassive = new UIMenuItem("Enable Passive Mode", "Enabling passive mode will mean people cannot attack you, you will also be unable to use weapons.");
         private UIMenuItem miPassive = new UIMenuItem("Enable Passive Mode", "Enabling passive mode will mean people cannot attack you, you will also be unable to use weapons.");
         private UIMenuItem miKillYourself = new UIMenuItem("Kill Yourself", "Kill yourself and respawn.");
 
