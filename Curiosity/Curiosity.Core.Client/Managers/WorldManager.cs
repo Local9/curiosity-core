b9277722c28d34d896dd8968dbcb54815dedf319
Antony Cook <kinglo0py@gmail.com>
2021-02-25 01:50:12 +0000

    Fix error with null check

--- Curiosity/Curiosity.Core.Client/Managers/WorldManager.cs
+++ Curiosity/Curiosity.Core.Client/Managers/WorldManager.cs
@@ -329,10 +329,9 @@ namespace Curiosity.Core.Client.Managers
                 {
                     if (vehiclesToSuppress.Contains((VehicleHash)veh.Model.Hash))
                     {
-                        if (veh.Exists())
-                        {
-                            if (!veh.State.Get(VehicleManager.STATE_VEH_SPAWNED)) veh.RemoveFromWorld();
-                        }
+                        bool serverSpawned = veh.State.Get(VehicleManager.STATE_VEH_SPAWNED) == null ? false : veh.State.Get(VehicleManager.STATE_VEH_SPAWNED);
+                        if (!serverSpawned)
+                            veh.RemoveFromWorld();
                     }
                 });
 
