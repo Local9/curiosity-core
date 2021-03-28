a9c8ff0093524f582db380fb078816ae215e863d
Antony Cook <kinglo0py@gmail.com>
2021-03-28 18:28:55 +0100

    Added screen fade in and out methods

---
+++ Curiosity/Curiosity.Core.Client/Utils/ScreenOptions.cs
@@ -0,0 +1,27 @@
+﻿using CitizenFX.Core;
+using System.Threading.Tasks;
+using static CitizenFX.Core.Native.API;
+
+namespace Curiosity.Core.Client.Utils
+{
+    public static class ScreenOptions
+    {
+        public static async Task ScreenFadeOut(int millis)
+        {
+            ScreenFadeOut(millis);
+            while(IsScreenFadingOut())
+            {
+                await BaseScript.Delay(10);
+            }
+        }
+
+        public static async Task ScreenFadeIn(int millis)
+        {
+            ScreenFadeIn(millis);
+            while (IsScreenFadingIn())
+            {
+                await BaseScript.Delay(10);
+            }
+        }
+    }
+}
