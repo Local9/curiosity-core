22be68c8f2c8b50f0990f6db6b582d75fb9c9bf7
Antony Cook <kinglo0py@gmail.com>
2021-04-20 22:31:23 +0100

    Remove defunc'd key

--- Curiosity/Curiosity.Systems.Library/DataKeys.cs
+++ Curiosity/Curiosity.Systems.Library/DataKeys.cs
@@ -3,6 +3,5 @@
     public class DataKeys
     {
         public const string SERVER_EVENT_KEY = "25cQDd5Mihk1hqLi9UDQbEP53";
-        public const string MISSION_EVENT_KEY = "z5Id5C72n3EILEH5W283837uy";
     }
 }
