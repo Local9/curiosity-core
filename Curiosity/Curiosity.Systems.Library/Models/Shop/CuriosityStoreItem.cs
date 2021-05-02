09077032083a4ddb8ba774bfa019fbf12459fdd3
Antony Cook <kinglo0py@gmail.com>
2021-05-02 15:49:23 +0100

    Added JSON conversion fields

--- Curiosity/Curiosity.Systems.Library/Models/Shop/CuriosityStoreItem.cs
+++ Curiosity/Curiosity.Systems.Library/Models/Shop/CuriosityStoreItem.cs
@@ -1,16 +1,53 @@
-﻿namespace Curiosity.Systems.Library.Models.Shop
+﻿using System.Runtime.Serialization;
+
+namespace Curiosity.Systems.Library.Models.Shop
 {
+    [DataContract]
     public class CuriosityStoreItem
     {
+        [DataMember(Name = "shopItemId")]
+        public int ShopItemId;
+
+        [DataMember(Name = "itemId")]
         public int ItemId;
+
+        [DataMember(Name = "label")]
         public string Label;
+
+        [DataMember(Name = "description")]
         public string Description;
+
+        [DataMember(Name = "buyValue")]
         public int BuyValue;
+
+        [DataMember(Name = "buyBackValue")]
         public int BuyBackValue;
+
+        [DataMember(Name = "numberInStock")]
         public int NumberInStock;
+
+        [DataMember(Name = "itemPurchased")]
         public bool ItemPurchased;
+
+        [DataMember(Name = "numberOwned")]
         public int NumberOwned;
-        public bool IsStockControlled;
+
+        [DataMember(Name = "isStockManaged")]
+        public bool IsStockManaged;
+
+        [DataMember(Name = "maximumAllowed")]
         public int MaximumAllowed;
+
+        [DataMember(Name = "hasRoleRequirement")]
+        public bool hasRoleRequirement;
+
+        [DataMember(Name = "numberOfSkillRequirements")]
+        public int NumberOfSkillRequirements;
+
+        [DataMember(Name = "numberOfItemRequirements")]
+        public int NumberOfItemRequirements;
+
+        [DataMember(Name = "imageUri")]
+        public string ImageUri;
     }
 }
