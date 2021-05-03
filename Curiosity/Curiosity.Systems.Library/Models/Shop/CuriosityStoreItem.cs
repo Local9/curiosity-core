355a928c6c57d824de4bba521e2a3b88f75f1441
Antony Cook <kinglo0py@gmail.com>
2021-05-03 15:37:33 +0100

    Add additional fields for items

--- Curiosity/Curiosity.Systems.Library/Models/Shop/CuriosityStoreItem.cs
+++ Curiosity/Curiosity.Systems.Library/Models/Shop/CuriosityStoreItem.cs
@@ -1,4 +1,5 @@
-﻿using System.Runtime.Serialization;
+﻿using System.Collections.Generic;
+using System.Runtime.Serialization;
 
 namespace Curiosity.Systems.Library.Models.Shop
 {
@@ -49,5 +50,41 @@ namespace Curiosity.Systems.Library.Models.Shop
 
         [DataMember(Name = "imageUri")]
         public string ImageUri;
+
+        [DataMember(Name = "skillRequirements")]
+        public List<SkillRequirement> SkillRequirements = new List<SkillRequirement>();
+
+        [DataMember(Name = "itemRequirements")]
+        public List<ItemRequirement> ItemRequirements = new List<ItemRequirement>();
+
+        [DataMember(Name = "roleRequirements")]
+        public List<RoleRequirement> RoleRequirements = new List<RoleRequirement>();
+    }
+
+    [DataContract]
+    public class SkillRequirement
+    {
+        public string SkillLabel;
+        public int ExperienceRequired;
+        public int ExperienceCurrent;
+        public bool RequirementMet;
+    }
+
+    [DataContract]
+    public class ItemRequirement
+    {
+        public string ItemLabel;
+        public int AmountRequired;
+        public int AmountCurrent;
+        public bool RequirementMet;
+    }
+
+    [DataContract]
+    public class RoleRequirement
+    {
+        public string ItemLabel;
+        public int RoleId;
+        public string Description;
+        public bool HasRole;
     }
 }
