e1572d6c57e7afd4a30b302a8465e53ba6492fa7
Antony Cook <kinglo0py@gmail.com>
2021-06-02 23:05:09 +0100

    uuurrrggggghhhhhhhh

--- Curiosity/Curiosity.Core.Server.2.0/Database/MySqlDatabase.cs
+++ Curiosity/Curiosity.Core.Server.2.0/Database/MySqlDatabase.cs
@@ -1,10 +1,16 @@
 ﻿using CitizenFX.Core;
 using CitizenFX.Core.Native;
+using Curiosity.Core.Server.Diagnostics;
+using Dapper;
 using GHMatti.Data.MySQL;
 using GHMatti.Data.MySQL.Core;
 using GHMatti.Utilities;
+using MySqlConnector;
+using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Xml.Linq;
 
 namespace Curiosity.Core.Server.Database
@@ -38,44 +44,61 @@ namespace Curiosity.Core.Server.Database
         }
     }
 
-    //public static class MySQLRefactor
-    //{
-    //    private static string _connectionString = API.GetConvar("mysql_connection_string", "");
+    public class MySqlRefactor : BaseScript
+    {
+        private static string _connectionString = API.GetConvar("mysql_connection_string", "");
 
-    //    public static async Task<dynamic> QueryAsync(string query, object parameters = null)
-    //    {
-    //        try
-    //        {
-    //            using (MySqlConnection _conn = new MySqlConnection(_connectionString))
-    //            {
-    //                CommandDefinition def = new CommandDefinition(query, parameters);
-    //                IEnumerable<dynamic> result = await _conn.QueryAsync<dynamic>(def);
-    //                await _conn.CloseAsync();
-    //                return result;
-    //            }
-    //        }
-    //        catch (Exception ex)
-    //        {
-    //            Logger.Error(ex.ToString());
-    //            return null;
-    //        }
-    //    }
+        public MySqlRefactor()
+        {
+            if (string.IsNullOrEmpty(_connectionString))
+            {
+                MySqlStringCheckError();
+            }
+        }
 
-    //    public static async Task ExecuteAsync(string query, object parameters)
-    //    {
-    //        try
-    //        {
-    //            using (MySqlConnection _conn = new MySqlConnection(_connectionString))
-    //            {
-    //                CommandDefinition def = new CommandDefinition(query, parameters);
-    //                await _conn.ExecuteAsync(def);
-    //                await _conn.CloseAsync();
-    //            }
-    //        }
-    //        catch (Exception ex)
-    //        {
-    //            Logger.Error(ex.ToString());
-    //        }
-    //    }
-    //}
+        private async void MySqlStringCheckError()
+        {
+            while(true)
+            {
+                Logger.Error($"mysql_connection_string is empty or missing");
+                await Delay(1000);
+            }
+        }
+
+        public async Task<dynamic> QueryAsync(string query, object parameters = null)
+        {
+            try
+            {
+                using (MySqlConnection _conn = new MySqlConnection(_connectionString))
+                {
+                    CommandDefinition def = new CommandDefinition(query, parameters);
+                    IEnumerable<dynamic> result = await _conn.QueryAsync<dynamic>(def);
+                    await _conn.CloseAsync();
+                    return result;
+                }
+            }
+            catch (Exception ex)
+            {
+                Logger.Error(ex.ToString());
+                return null;
+            }
+        }
+
+        public async Task ExecuteAsync(string query, object parameters)
+        {
+            try
+            {
+                using (MySqlConnection _conn = new MySqlConnection(_connectionString))
+                {
+                    CommandDefinition def = new CommandDefinition(query, parameters);
+                    await _conn.ExecuteAsync(def);
+                    await _conn.CloseAsync();
+                }
+            }
+            catch (Exception ex)
+            {
+                Logger.Error(ex.ToString());
+            }
+        }
+    }
 }
---
+++ Curiosity/Curiosity.Core.Server.2.0/fxmanifest.lua
@@ -0,0 +1,13 @@
+﻿fx_version 'adamant'
+game 'gta5'
+
+files {
+  'config/**.json',
+  'Newtonsoft.Json.dll',
+}
+
+server_scripts {
+  '*.Library.net.dll',
+  '*.Shared.net.dll',
+  '*.Server.net.dll',
+}
