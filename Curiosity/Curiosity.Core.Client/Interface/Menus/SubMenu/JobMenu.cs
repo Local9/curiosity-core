b625234a7b8f46de79b92d54dcc24b1883d4d004
Antony Cook <kinglo0py@gmail.com>
2021-06-02 23:34:04 +0100

    Initial start on job menu (idea)

---
+++ Curiosity/Curiosity.Core.Client/Interface/Menus/SubMenu/JobMenu.cs
@@ -0,0 +1,29 @@
+﻿using NativeUI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Curiosity.Core.Client.Interface.Menus.SubMenu
+{
+    class JobMenu
+    {
+        private UIMenu jobMenu;
+
+        public UIMenu CreateMenu(UIMenu menu)
+        {
+            menu.OnMenuOpen += Menu_OnMenuOpen;
+
+            menu.MouseControlsEnabled = false;
+            menu.MouseEdgeEnabled = false;
+
+            return menu;
+        }
+
+        private void Menu_OnMenuOpen(UIMenu menu)
+        {
+
+        }
+    }
+}
