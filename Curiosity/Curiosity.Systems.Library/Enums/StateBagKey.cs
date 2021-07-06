1178d4486f6459883fb1cfd0f1172da637171534
Antony Cook <kinglo0py@gmail.com>
2021-07-06 21:50:10 +0100

    Update StateBag to be strings

--- Curiosity/Curiosity.Systems.Library/Enums/StateBagKey.cs
+++ Curiosity/Curiosity.Systems.Library/Enums/StateBagKey.cs
@@ -1,31 +1,31 @@
 ﻿namespace Curiosity.Systems.Library.Enums
 {
-    public enum StateBagKey
+    public static class StateBagKey
     {
-        PLAYER_NAME ,
-        SERVER_HANDLE,
-        PLAYER_CASH,
-        PLAYER_VEHICLE,
-        STAFF_MEMBER,
-        PLAYER_FREEZE,
-        VEH_SPAWNED,
-        VEH_OWNER_ID,
-        VEH_OWNER,
-        VEH_SPAWN_TYPE,
-        VEH_PERSONAL,
-        VEH_PERSONAL_TRAILER,
-        BLIP_ID,
-        VEH_FUEL_MULTIPLIER,
-        VEH_FUEL,
-        VEH_FUEL_SETUP,
-        VEH_CONTENT,
-        VEH_TRAILER_CONTENT,
-        BLIP_INFORMATION,
-        VEH_NETWORK_ID,
-        VEH_TRAILER_NETWORK_ID,
-        VEH_HELI_NETWORK_ID,
-        VEH_PLANE_NETWORK_ID,
-        VEH_BOAT_NETWORK_ID,
-        PLAYER_ROUTING
+        public const string PLAYER_NAME = "PLAYER_NAME";
+        public const string SERVER_HANDLE = "SERVER_HANDLE";
+        public const string PLAYER_CASH = "PLAYER_CASH";
+        public const string PLAYER_VEHICLE = "PLAYER_VEHICLE";
+        public const string STAFF_MEMBER = "STAFF_MEMBER";
+        public const string PLAYER_FREEZE = "PLAYER_FREEZE";
+        public const string VEH_SPAWNED = "VEH_SPAWNED";
+        public const string VEH_OWNER_ID = "VEH_OWNER_ID";
+        public const string VEH_OWNER = "VEH_OWNER";
+        public const string VEH_SPAWN_TYPE = "VEH_SPAWN_TYPE";
+        public const string VEH_PERSONAL = "VEH_PERSONAL";
+        public const string VEH_PERSONAL_TRAILER = "VEH_PERSONAL_TRAILER";
+        public const string BLIP_ID = "BLIP_ID";
+        public const string VEH_FUEL_MULTIPLIER = "VEH_FUEL_MULTIPLIER";
+        public const string VEH_FUEL = "VEH_FUEL";
+        public const string VEH_FUEL_SETUP = "VEH_FUEL_SETUP";
+        public const string VEH_CONTENT = "VEH_CONTENT";
+        public const string VEH_TRAILER_CONTENT = "VEH_TRAILER_CONTENT";
+        public const string BLIP_INFORMATION = "BLIP_INFORMATION";
+        public const string VEH_NETWORK_ID = "VEH_NETWORK_ID";
+        public const string VEH_TRAILER_NETWORK_ID = "VEH_TRAILER_NETWORK_ID";
+        public const string VEH_HELI_NETWORK_ID = "VEH_HELI_NETWORK_ID";
+        public const string VEH_PLANE_NETWORK_ID = "VEH_PLANE_NETWORK_ID";
+        public const string VEH_BOAT_NETWORK_ID = "VEH_BOAT_NETWORK_ID";
+        public const string PLAYER_ROUTING = "PLAYER_ROUTING";
     }
 }
