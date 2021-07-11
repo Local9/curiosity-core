8e0715645d6534d4448435e53e20c125b90cf7b0
Antony Cook <kinglo0py@gmail.com>
2021-07-12 00:17:00 +0100

    remove using

--- Curiosity/Curiosity.Core.Client/Interface/Menus/Creator/CharacterAppearance.cs
+++ Curiosity/Curiosity.Core.Client/Interface/Menus/Creator/CharacterAppearance.cs
@@ -1,6 +1,5 @@
 ﻿using CitizenFX.Core;
 using CitizenFX.Core.Native;
-using Curiosity.Core.Client.Diagnostics;
 using Curiosity.Core.Client.Environment.Entities.Models;
 using Curiosity.Core.Client.Extensions;
 using NativeUI;
