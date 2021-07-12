c4faddb006fb4943b6f67e8d576c90815c4446c7
Antony Cook <kinglo0py@gmail.com>
2021-07-12 14:56:53 +0100

    Added exports for Discord Presence

--- Curiosity/Curiosity.Core.Client/ClientExports/DiscordExports.cs
+++ Curiosity/Curiosity.Core.Client/ClientExports/DiscordExports.cs
@@ -7,6 +7,26 @@ namespace Curiosity.Core.Client.ClientExports
     {
         public override void Begin()
         {
+            Instance.ExportDictionary.Add("DiscordSetAsset", new Func<string, string, string>(
+                (asset, text) =>
+                {
+                    Instance.DiscordRichPresence.Asset = asset;
+                    Instance.DiscordRichPresence.AssetText = text;
+                    Instance.DiscordRichPresence.Commit();
+                    return null;
+                }
+            ));
+
+            Instance.ExportDictionary.Add("DiscordSetSmallAsset", new Func<string, string, string>(
+                (asset, text) =>
+                {
+                    Instance.DiscordRichPresence.SmallAsset = asset;
+                    Instance.DiscordRichPresence.SmallAssetText = text;
+                    Instance.DiscordRichPresence.Commit();
+                    return null;
+                }
+            ));
+
             Instance.ExportDictionary.Add("DiscordSetStatus", new Func<string, string>(
                 (status) =>
                 {
