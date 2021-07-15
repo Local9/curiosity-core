acc65ad132b39eac390a0af1522df75366db9101
Antony Cook <kinglo0py@gmail.com>
2021-07-15 18:23:56 +0100

    Send players to routing bucket 0 for lobby

--- Curiosity/Curiosity.Systems.Library/Enums/RoutingBucket.cs
+++ Curiosity/Curiosity.Systems.Library/Enums/RoutingBucket.cs
@@ -3,9 +3,9 @@
     public enum RoutingBucket
     {
         CHARACTER_LOADING = 5000000,
-        LOBBY = 1,
-        CAYO_ISLAND = 2,
-        RACING = 3,
-        TRUCKING = 4,
+        LOBBY = 0,
+        CAYO_ISLAND = 1,
+        RACING = 2,
+        TRUCKING = 3,
     }
 }
