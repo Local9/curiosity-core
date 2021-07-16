24214be2d9a21f79b1f18cdf41c24e02fdcec1f9
Antony Cook <kinglo0py@gmail.com>
2021-07-17 00:54:33 +0100

    Weapon IsReload Testing

--- Curiosity/Curiosity.Core.Client/Managers/WeaponManager.cs
+++ Curiosity/Curiosity.Core.Client/Managers/WeaponManager.cs
@@ -1,10 +1,32 @@
-﻿namespace Curiosity.Core.Client.Managers
+﻿using Curiosity.Core.Client.Diagnostics;
+using System.Threading.Tasks;
+
+namespace Curiosity.Core.Client.Managers
 {
     public class WeaponManager : Manager<WeaponManager>
     {
+        bool isReloadingCheck = false;
+
         public override void Begin()
         {
             
+        }
+
+        [TickHandler(SessionWait = true)]
+        private async Task OnReloadTick()
+        {
+            if (Cache.PlayerPed.IsReloading)
+            {
+                if (isReloadingCheck) return;
+                
+                int ammoCount = Cache.PlayerPed.Weapons.Current.Ammo;
+
+                Logger.Debug($"Player is Reloading, current ammo {ammoCount}");
+            }
+            else
+            {
+                isReloadingCheck = false;
+            }
         }
     }
 }
