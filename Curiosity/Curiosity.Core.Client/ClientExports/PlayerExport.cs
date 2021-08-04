1cb0eeec67c71a68f1b829de35c16bb99a96a046
Antony Cook <kinglo0py@gmail.com>
2021-08-04 20:23:20 +0100

    Add client export, passive

---
+++ Curiosity/Curiosity.Core.Client/ClientExports/PlayerExport.cs
@@ -0,0 +1,24 @@
+﻿using CitizenFX.Core;
+using Curiosity.Core.Client.Managers;
+using Curiosity.Systems.Library.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Curiosity.Core.Client.ClientExports
+{
+    public class PlayerExport : Manager<PlayerExport>
+    {
+        public override void Begin()
+        {
+            Instance.ExportDictionary.Add("Passive", new Func<bool>(
+                () =>
+                {
+                    return Game.Player.State.Get(StateBagKey.PLAYER_PASSIVE) ?? false;
+                }
+            ));
+        }
+    }
+}
