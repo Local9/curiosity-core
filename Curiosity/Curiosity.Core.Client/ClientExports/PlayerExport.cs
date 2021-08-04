eb95cd875441990c14a5bb6c3e64e55be3cf5e5c
Antony Cook <kinglo0py@gmail.com>
2021-08-04 20:23:53 +0100

    clean usings

--- Curiosity/Curiosity.Core.Client/ClientExports/PlayerExport.cs
+++ Curiosity/Curiosity.Core.Client/ClientExports/PlayerExport.cs
@@ -2,10 +2,6 @@
 using Curiosity.Core.Client.Managers;
 using Curiosity.Systems.Library.Enums;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Curiosity.Core.Client.ClientExports
 {
