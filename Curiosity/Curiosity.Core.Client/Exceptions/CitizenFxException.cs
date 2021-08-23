bc886de040ca68e30a25c85409ffc6856edc2a47
Antony Cook <kinglo0py@gmail.com>
2021-08-23 14:13:36 +0100

    Added CitizenFX Exception

---
+++ Curiosity/Curiosity.Core.Client/Exceptions/CitizenFxException.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Curiosity.Core.Client.Exceptions
+{
+    class CitizenFxException : Exception
+    {
+        public CitizenFxException()
+        {
+
+        }
+
+        public CitizenFxException(string message) : base(message)
+        {
+
+        }
+
+        public CitizenFxException(string message, Exception innerException) : base(message, innerException)
+        {
+
+        }
+
+        protected CitizenFxException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+
+        }
+    }
+}
