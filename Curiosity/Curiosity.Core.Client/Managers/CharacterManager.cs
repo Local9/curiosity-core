365652aeafe2b210f5fb63a28fb10d2944e9e565
Antony Cook <kinglo0py@gmail.com>
2021-08-27 01:15:24 +0100

    check PMA exists before calling it

--- Curiosity/Curiosity.Core.Client/Managers/CharacterManager.cs
+++ Curiosity/Curiosity.Core.Client/Managers/CharacterManager.cs
@@ -118,7 +118,8 @@ namespace Curiosity.Core.Client.Managers
         {
             Screen.Fading.FadeIn(0);
 
-            Instance.ExportDictionary["pma-voice"].toggleMute();
+            if (Instance.ExportDictionary["pma-voice"] is not null)
+                Instance.ExportDictionary["pma-voice"].toggleMute();
 
             API.SetTimecycleModifier("default");
             API.SetTimecycleModifierStrength(1f);
@@ -309,7 +310,9 @@ namespace Curiosity.Core.Client.Managers
             player.EnableHud();
 
             EventSystem.Send("character:routing:base");
-            Instance.ExportDictionary["pma-voice"].toggleMute();
+
+            if (Instance.ExportDictionary["pma-voice"] is not null)
+                Instance.ExportDictionary["pma-voice"].toggleMute();
 
             PlayerOptionsManager.GetModule().SetPlayerPassiveOnStart(Cache.Character.IsPassive);
             Logger.Debug($"Character Passive State: {Cache.Character.IsPassive}");
