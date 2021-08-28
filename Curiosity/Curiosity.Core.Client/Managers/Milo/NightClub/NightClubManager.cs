41a70c4c4b6e1af10c1934ebecafa468f06e7def
Antony Cook <kinglo0py@gmail.com>
2021-08-28 21:32:14 +0100

    Effects Manager "completed"

---
+++ Curiosity/Curiosity.Core.Client/Managers/Milo/NightClub/NightClubManager.cs
@@ -0,0 +1,23 @@
+﻿using Curiosity.Systems.Library.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Curiosity.Core.Client.Managers.Milo.NightClub
+{
+    public class NightClubManager : Manager<NightClubManager>
+    {
+        /// <summary>
+        ///  https://github.com/TomGrobbe/NightClubs
+        ///  TOM YOU'RE A FUCKING LEGEND!
+        /// </summary>
+        /// 
+
+        public Position posEnterance = new Position(194.6124f, -3167.278f, 5.790269f, 86.42644f);
+        public Position posExit = new Position(-1569.665f, -3016.758f, -74.40615f, 86.42644f);
+
+
+    }
+}
