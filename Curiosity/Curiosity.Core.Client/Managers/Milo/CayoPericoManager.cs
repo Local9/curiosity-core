4c0e87cc73b8ca9c236fb3a3a65f28bffc2177ee
Antony Cook <kinglo0py@gmail.com>
2021-08-28 22:22:12 +0100

    increase marker size

--- Curiosity/Curiosity.Core.Client/Managers/Milo/CayoPericoManager.cs
+++ Curiosity/Curiosity.Core.Client/Managers/Milo/CayoPericoManager.cs
@@ -39,7 +39,7 @@ namespace Curiosity.Core.Client.Managers.Milo
         Position posCayoVehicle = new Position(4058.679f, -4674.118f, 4.184523f, 28.08182f);
 
         Color markerColor = Color.FromArgb(255, 135, 206, 235);
-        Vector3 markerScale = new Vector3(1f, 1f, .5f);
+        Vector3 markerScale = new Vector3(1.5f, 1.5f, .5f);
         Vector3 markerScaleVehicle = new Vector3(10f, 10f, 1f);
 
         NUIMarker markerLs1;
