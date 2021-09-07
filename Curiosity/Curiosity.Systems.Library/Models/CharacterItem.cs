7c102658bddf820bf501b86a301c05b20b286444
Antony Cook <kinglo0py@gmail.com>
2021-09-07 23:51:11 +0100

    Add Character Item

---
+++ Curiosity/Curiosity.Systems.Library/Models/CharacterItem.cs
@@ -0,0 +1,20 @@
+﻿using System.Runtime.Serialization;
+
+namespace Curiosity.Systems.Library.Models
+{
+    [DataContract]
+    public class CharacterItem
+    {
+        [DataMember(Name = "shopCategory", EmitDefaultValue = false)]
+        public string ShopCategory;
+
+        [DataMember(Name = "itemCategory", EmitDefaultValue = false)]
+        public string ItemCategory;
+
+        [DataMember(Name = "label", EmitDefaultValue = false)]
+        public string Label;
+
+        [DataMember(Name = "numberOwned", EmitDefaultValue = false)]
+        public int NumberOwned;
+    }
+}
