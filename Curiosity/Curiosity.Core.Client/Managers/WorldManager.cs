ec754334e896b738d6ac6635e09065809ec0965a
Antony Cook <kinglo0py@gmail.com>
2021-09-15 23:50:26 +0100

    Close the bold tag

--- Curiosity/Curiosity.Core.Client/Managers/WorldManager.cs
+++ Curiosity/Curiosity.Core.Client/Managers/WorldManager.cs
@@ -162,7 +162,7 @@ namespace Curiosity.Core.Client.Managers
                 int interiorId = GetInteriorFromEntity(PlayerPedId());
 
                 if (interiorId == 0)
-                    NotificationManager.GetModule().Info($"<b>Weather Update<b><br /><b>Area</b>: {area}<br />{GetForecastText(weatherType)}");
+                    NotificationManager.GetModule().Info($"<b>Weather Update</b><br /><b>Area</b>: {area}<br />{GetForecastText(weatherType)}");
                 
                 await BaseScript.Delay(15000);
 
