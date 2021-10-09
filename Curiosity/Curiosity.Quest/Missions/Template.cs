c1e60155eea168757cd15effd8730c69e3647a6c
Antony Cook <kinglo0py@gmail.com>
2021-10-09 15:12:49 +0100

    Added Quest mission

---
+++ Curiosity/Curiosity.Quest/Missions/Template.cs
@@ -0,0 +1,29 @@
+﻿using Curiosity.MissionManager.Client;
+using Curiosity.MissionManager.Client.Attributes;
+using Curiosity.Systems.Library.Enums;
+using System.Threading.Tasks;
+using Ped = Curiosity.MissionManager.Client.Classes.Ped;
+using Vehicle = Curiosity.MissionManager.Client.Classes.Vehicle;
+
+namespace Curiosity.Quest.Missions
+{
+    [MissionInfo("Template", "Template", 0f, 0f, 0f, MissionType.Mission, true, "None")]
+    public class Template : Mission
+    {
+        public override void Start()
+        {
+
+            MissionManager.Instance.RegisterTickHandler(OnMissionTick);
+        }
+
+        public override void End()
+        {
+            MissionManager.Instance.DeregisterTickHandler(OnMissionTick);
+        }
+
+        async Task OnMissionTick()
+        {
+
+        }
+    }
+}
