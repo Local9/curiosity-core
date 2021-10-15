9f277741e2464b382323818e3ef392741773b223
Antony Cook <kinglo0py@gmail.com>
2021-10-15 23:39:22 +0100

    NUIMarker can have a blip attached to it

--- Curiosity/Curiosity.MissionManager.Client/Extensions/NativeUIExtensions.cs
+++ Curiosity/Curiosity.MissionManager.Client/Extensions/NativeUIExtensions.cs
@@ -1,12 +1,14 @@
 ﻿using CitizenFX.Core;
-using NativeUI;
+using Curiosity.MissionManager.Client.Environment.Entities.Models;
 using System.Drawing;
 
 namespace Curiosity.MissionManager.Client.Extensions
 {
-    public class NUIMarker : Marker
+    public class NUIMarker : NativeUI.Marker
     {
         public dynamic Data;
+        public Blip Blip;
+        public BlipMissionInfo BlipMissionInfo;
 
         public NUIMarker(MarkerType type, Vector3 position, float distance, Color color, bool placeOnGround = false, bool bobUpDown = false, bool rotate = false, bool faceCamera = false) : base(type, position, distance, color, placeOnGround, bobUpDown, rotate, faceCamera)
         {
