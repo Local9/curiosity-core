d391d74134f91f2756514416909772c3f3ea04a9
Antony Cook <kinglo0py@gmail.com>
2021-11-06 11:58:01 +0000

    Remove dead code

--- Curiosity/Curiosity.Core.Client/Managers/UI/PlayerNameManager.cs
+++ Curiosity/Curiosity.Core.Client/Managers/UI/PlayerNameManager.cs
@@ -10,7 +10,6 @@ namespace Curiosity.Core.Client.Managers.UI
 {
     public class PlayerNameManager : Manager<PlayerNameManager>
     {
-        Dictionary<int, PlayerNameTag> currentPlayerNameTags = new Dictionary<int, PlayerNameTag>();
         Dictionary<Player, int> gamerTags = new Dictionary<Player, int>();
 
         public int NameTagColor = 0;
@@ -24,32 +23,6 @@ namespace Curiosity.Core.Client.Managers.UI
         {
 
         }
-
-        //[TickHandler(SessionWait = true)]
-        //private async Task OnDebugColor()
-        //{
-        //    if (Utils.ControlHelper.IsControlJustPressed(Control.PhoneUp))
-        //    {
-        //        NameTagColor++;
-
-        //        if (NameTagColor >= 255)
-        //            NameTagColor = 0;
-
-        //        await BaseScript.Delay(100);
-        //    }
-
-        //    if (Utils.ControlHelper.IsControlJustPressed(Control.PhoneDown))
-        //    {
-        //        NameTagColor--;
-
-        //        if (NameTagColor < 0)
-        //            NameTagColor = 255;
-
-        //        await BaseScript.Delay(100);
-        //    }
-
-        //    Screen.ShowSubtitle($"Debug Color: {NameTagColor}");
-        //}
 
         [TickHandler(SessionWait = true)]
         private Task OnPlayerOverheadNamesTask()
@@ -137,117 +110,6 @@ namespace Curiosity.Core.Client.Managers.UI
             return BaseScript.Delay(500);
         }
 
-        //private Task OnWorldPlayerNames()
-        //{
-        //    foreach (Player player in Instance.PlayerList)
-        //    {
-        //        int playerHandle = player.Handle;
-        //        int pedHandle = player.Character.Handle;
-
-        //        if (player == Game.Player && !ShowMyName)
-        //        {
-        //            if (currentPlayerNameTags.ContainsKey(playerHandle))
-        //            {
-        //                // RemoveAndHideNameTag(currentPlayerNameTags[playerHandle], playerHandle);
-        //            }
-        //            continue;
-        //        }
-
-        //        if (NetworkIsPlayerActive(playerHandle) && ShowPlayerNames)
-        //        {
-        //            if (!currentPlayerNameTags.ContainsKey(playerHandle))
-        //            {
-        //                string playerName = $"{GetPlayerName(playerHandle)}";
-        //                if (ShowServerHandle)
-        //                {
-        //                    playerName += $" [{GetPlayerServerId(playerHandle)}]";
-        //                }
-
-        //                if (currentPlayerNameTags.ContainsKey(playerHandle))
-        //                {
-        //                    RemoveMpGamerTag(currentPlayerNameTags[playerHandle].TagHandle);
-        //                }
-
-        //                PlayerNameTag playerNameTag = new PlayerNameTag();
-
-        //                bool isStaff = player.State.Get($"{StateBagKey.STAFF_MEMBER}") == null ? false : player.State.Get($"{StateBagKey.STAFF_MEMBER}");
-
-        //                if (isStaff)
-        //                    StaffStarColor = 64;
-
-        //                playerNameTag.TagHandle = CreateMpGamerTag(pedHandle, playerName, false, isStaff, string.Empty, 0);
-        //                //SetMpGamerTagVisibility(playerNameTag.TagHandle, (int)GamerTagComponent.GamerName, true);
-
-        //                SetMpGamerTagVisibility(playerNameTag.TagHandle, (int)GamerTagComponent.AudioIcon, NetworkIsPlayerTalking(playerHandle));
-        //                SetMpGamerTagColour(playerNameTag.TagHandle, (int)GamerTagComponent.AudioIcon, 208);
-        //                SetMpGamerTagAlpha(playerNameTag.TagHandle, (int)GamerTagComponent.AudioIcon, 255);
-
-        //                //SetMpGamerTagVisibility(playerNameTag.TagHandle, (int)GamerTagComponent.CrewTag, true);
-        //                //SetMpGamerTagVisibility(playerNameTag.TagHandle, (int)GamerTagComponent.HealthArmour, false);
-        //                //SetMpGamerTagVisibility(playerNameTag.TagHandle, (int)GamerTagComponent.BigText, false);
-        //                //SetMpGamerTagVisibility(playerNameTag.TagHandle, (int)GamerTagComponent.MpUsingMenu, false);
-        //                bool isPassive = player.State.Get(StateBagKey.PLAYER_PASSIVE) ?? false;
-        //                SetMpGamerTagVisibility(playerNameTag.TagHandle, (int)GamerTagComponent.MpPassiveMode, isPassive);
-        //                SetMpGamerTagVisibility(playerNameTag.TagHandle, (int)GamerTagComponent.WantedStars, isStaff);
-
-        //                bool isDriving = false;
-
-        //                if (IsPedSittingInAnyVehicle(pedHandle))
-        //                {
-        //                    isDriving = player.Character.CurrentVehicle.Driver.Handle == pedHandle;
-        //                }
-
-        //                SetMpGamerTagVisibility(playerNameTag.TagHandle, (int)GamerTagComponent.MpDriver, isDriving);
-
-        //                //SetMpGamerTagVisibility(playerNameTag.TagHandle, (int)GamerTagComponent.MpCoDriver, false);
-        //                //SetMpGamerTagVisibility(playerNameTag.TagHandle, (int)GamerTagComponent.MpTagged, false);
-        //                //SetMpGamerTagVisibility(playerNameTag.TagHandle, (int)GamerTagComponent.GamerNameNearby, false);
-        //                //SetMpGamerTagVisibility(playerNameTag.TagHandle, (int)GamerTagComponent.Arrow, false);
-        //                //SetMpGamerTagVisibility(playerNameTag.TagHandle, (int)GamerTagComponent.MpPackages, false);
-        //                //SetMpGamerTagVisibility(playerNameTag.TagHandle, (int)GamerTagComponent.InvIfPedFollowing, false);
-        //                //SetMpGamerTagVisibility(playerNameTag.TagHandle, (int)GamerTagComponent.RankText, false);
-        //                //SetMpGamerTagVisibility(playerNameTag.TagHandle, (int)GamerTagComponent.MpTyping, false);
-        //                //SetMpGamerTagVisibility(playerNameTag.TagHandle, (int)GamerTagComponent.MpBagLarge, false);
-        //                //SetMpGamerTagVisibility(playerNameTag.TagHandle, (int)GamerTagComponent.MpTagArrow, false);
-        //                //SetMpGamerTagVisibility(playerNameTag.TagHandle, (int)GamerTagComponent.MpGangCeo, false);
-        //                //SetMpGamerTagVisibility(playerNameTag.TagHandle, (int)GamerTagComponent.MpGangBiker, false);
-        //                //SetMpGamerTagVisibility(playerNameTag.TagHandle, (int)GamerTagComponent.BikerArrow, false);
-        //                //SetMpGamerTagVisibility(playerNameTag.TagHandle, (int)GamerTagComponent.McRolePresident, false);
-        //                //SetMpGamerTagVisibility(playerNameTag.TagHandle, (int)GamerTagComponent.McRoleVicePresident, false);
-        //                //SetMpGamerTagVisibility(playerNameTag.TagHandle, (int)GamerTagComponent.McRoleRoadCaptain, false);
-        //                //SetMpGamerTagVisibility(playerNameTag.TagHandle, (int)GamerTagComponent.McRoleSargeant, false);
-        //                //SetMpGamerTagVisibility(playerNameTag.TagHandle, (int)GamerTagComponent.McRoleEnforcer, false);
-        //                //SetMpGamerTagVisibility(playerNameTag.TagHandle, (int)GamerTagComponent.McRoleProspect, false);
-        //                //SetMpGamerTagVisibility(playerNameTag.TagHandle, (int)GamerTagComponent.MpTransmitter, false);
-        //                //SetMpGamerTagVisibility(playerNameTag.TagHandle, (int)GamerTagComponent.MpBomb, false);
-
-        //                SetMpGamerTagIcons(playerNameTag.TagHandle, false);
-        //                SetMpGamerTagColour(playerNameTag.TagHandle, (int)GamerTagComponent.WantedStars, StaffStarColor);
-        //                //SetMpGamerTagBigText(playerNameTag.TagHandle, "BIG TEXT");
-        //                //SetMpGamerTagChatting(playerNameTag.TagHandle, "typing...");
-
-        //                NameTagColor = 0;
-        //                if (GetPlayerWantedLevel(playerHandle) > 0)
-        //                    NameTagColor = 6;
-
-        //                SetMpGamerTagColour(playerNameTag.TagHandle, (int)GamerTagComponent.GamerName, NameTagColor);
-
-        //                playerNameTag.PedHandle = pedHandle;
-
-        //            }
-        //            else if (!ShowPlayerNames)
-        //            {
-        //                // RemoveAndHideNameTag(currentPlayerNameTags[playerHandle], playerHandle);
-        //            }
-        //        }
-        //        else if (currentPlayerNameTags.ContainsKey(playerHandle))
-        //        {
-        //            // RemoveAndHideNameTag(currentPlayerNameTags[playerHandle], playerHandle);
-        //        }
-        //    }
-        //    return BaseScript.Delay(500);
-        //}
-
         void RemoveAndHideNameTag(Player player, int tagHandle)
         {
             SetMpGamerTagEnabled(tagHandle, false);
@@ -269,11 +131,6 @@ namespace Curiosity.Core.Client.Managers.UI
                     blip.Sprite = BlipSprite.Standard;
                     blip.Scale = 0.85f;
 
-                    SetBlipCategory(blip.Handle, 7);
-                    SetBlipPriority(blip.Handle, 11);
-                    ShowHeadingIndicatorOnBlip(blip.Handle, true);
-                    SetBlipNameToPlayerName(blip.Handle, player.Handle);
-
                     string key = $"playerBlip{player.ServerId}";
                     AddTextEntry(key, player.Name);
 
@@ -281,11 +138,23 @@ namespace Curiosity.Core.Client.Managers.UI
                     BeginTextCommandSetBlipName(key);
                     EndTextCommandSetBlipName(blip.Handle);
 
+                    SetBlipCategory(blip.Handle, 7);
+                    SetBlipPriority(blip.Handle, 11);
+                    ShowHeadingIndicatorOnBlip(blip.Handle, true);
+                    SetBlipNameToPlayerName(blip.Handle, player.Handle);
+
                     blip.Name = player.Name;
                 }
                 else
                 {
                     blip = player.Character.AttachedBlip;
+
+                    string key = $"playerBlip{player.ServerId}";
+                    AddTextEntry(key, player.Name);
+
+                    AddTextComponentSubstringBlipName(blip.Handle);
+                    BeginTextCommandSetBlipName(key);
+                    EndTextCommandSetBlipName(blip.Handle);
 
                     SetBlipCategory(blip.Handle, 7);
                     SetBlipPriority(blip.Handle, 11);
@@ -338,11 +207,5 @@ namespace Curiosity.Core.Client.Managers.UI
                 }
             }
         }
-    }
-
-    public class PlayerNameTag
-    {
-        public int TagHandle;
-        public int PedHandle;
     }
 }
