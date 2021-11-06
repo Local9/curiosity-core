c881df0aa2b1be3f5262473f6b9e6ab4a6f0cfb2
Antony Cook <kinglo0py@gmail.com>
2021-11-06 17:34:47 +0000

    inital work on passive phasing

--- Curiosity/Curiosity.Core.Client/Environment/Entities/WorldPlayer.cs
+++ Curiosity/Curiosity.Core.Client/Environment/Entities/WorldPlayer.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Curiosity.Core.Client.Managers;
+using Curiosity.Core.Client.Diagnostics;
 
 namespace Curiosity.Core.Client.Environment.Entities
 {
@@ -32,6 +33,27 @@ namespace Curiosity.Core.Client.Environment.Entities
         {
             PluginManager.DetachTickHandler(OnPlayerPassive);
             RemoveStateBagChangeHandler(isPassiveStateBagHandler);
+
+            bool playerInVehicle = PlayerPed.IsInVehicle();
+            bool currentPlayerInVehicle = Game.PlayerPed.IsInVehicle();
+
+            if (playerInVehicle)
+            {
+                PlayerPed.CurrentVehicle.ResetOpacity();
+                PlayerPed.CurrentVehicle.SetNoCollision(Game.PlayerPed, true);
+            }
+
+            if (currentPlayerInVehicle)
+            {
+                Game.PlayerPed.CurrentVehicle.ResetOpacity();
+                Game.PlayerPed.CurrentVehicle.SetNoCollision(PlayerPed, true);
+            }
+
+            if (playerInVehicle && currentPlayerInVehicle)
+            {
+                Game.PlayerPed.CurrentVehicle.SetNoCollision(PlayerPed.CurrentVehicle, true);
+                PlayerPed.CurrentVehicle.SetNoCollision(Game.PlayerPed.CurrentVehicle, true);
+            }
         }
 
         private void OnStatePlayerPassiveChange(string bag, string key, dynamic isPassive, int reserved, bool replicated)
@@ -45,16 +67,22 @@ namespace Curiosity.Core.Client.Environment.Entities
             {
                 if (IsPassive || PlayerOptions.IsPassive)
                 {
-                    if (Game.PlayerPed.IsInRangeOf(PlayerPed.Position, 35))
+                    if (Game.PlayerPed.IsInRangeOf(PlayerPed.Position, 15))
                     {
                         bool playerInVehicle = PlayerPed.IsInVehicle();
                         bool currentPlayerInVehicle = Game.PlayerPed.IsInVehicle();
 
                         if (playerInVehicle)
+                        {
+                            PlayerPed.CurrentVehicle.Opacity = 200;
                             PlayerPed.CurrentVehicle.SetNoCollision(Game.PlayerPed, false);
+                        }
 
                         if (currentPlayerInVehicle)
+                        {
+                            Game.PlayerPed.CurrentVehicle.Opacity = 200;
                             Game.PlayerPed.CurrentVehicle.SetNoCollision(PlayerPed, false);
+                        }
 
                         if (playerInVehicle && currentPlayerInVehicle)
                         {
@@ -66,7 +94,7 @@ namespace Curiosity.Core.Client.Environment.Entities
             }
             catch (Exception ex)
             {
-                // just don't crash
+                Logger.Debug(ex, $"OnPlayerPassive");
             }
         }
     }
---
+++ Curiosity/Curiosity.Core.Client/Managers/GameWorld/WorldPlayerPassiveManager.cs
@@ -0,0 +1,72 @@
+﻿using CitizenFX.Core;
+using Curiosity.Core.Client.Diagnostics;
+using Curiosity.Core.Client.Environment.Entities;
+using Curiosity.Core.Client.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Curiosity.Core.Client.Managers.GameWorld
+{
+    public class WorldPlayerPassiveManager : Manager<WorldPlayerPassiveManager>
+    {
+        Dictionary<int, WorldPlayer> players = new Dictionary<int, WorldPlayer>();
+
+        public override void Begin()
+        {
+            
+        }
+
+        [TickHandler(SessionWait = true)]
+        private async Task OnWorldPlayerPassiveTick()
+        {
+            try
+            {
+                foreach (Player player in Instance.PlayerList)
+                {
+                    if (player == Game.Player) continue;
+
+                    if (players.ContainsKey(player.ServerId)) continue;
+
+                    players.Add(player.ServerId, new WorldPlayer(player));
+                }
+
+                foreach (KeyValuePair<int, WorldPlayer> keyValuePair in players.ToArray())
+                {
+                    int serverHandle = keyValuePair.Key;
+                    WorldPlayer player = keyValuePair.Value;
+
+                    if (player.Player is null)
+                    {
+                        goto REMOVE_PLAYER;
+                    }
+
+                    if (player.Player.Character.Exists())
+                    {
+                        if (player.Player.Character.IsDead)
+                        {
+                            goto REMOVE_PLAYER;
+                        }
+                    }
+
+                    if (Game.PlayerPed.Position.Distance(player.Player.Character.Position) > 30)
+                    {
+                        goto REMOVE_PLAYER;
+                    }
+
+                    continue;
+
+                REMOVE_PLAYER:
+                    player.Dispose();
+                    players.Remove(serverHandle);
+                }
+            }
+            catch(Exception ex)
+            {
+                Logger.Debug(ex, $"OnWorldPlayerPassiveTick");
+            }
+        }
+    }
+}
