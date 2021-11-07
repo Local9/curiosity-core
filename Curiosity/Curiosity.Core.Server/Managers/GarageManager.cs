47595c364200e45619ec99911b4dccb3177d4fe4
Antony Cook <kinglo0py@gmail.com>
2021-11-07 17:18:04 +0000

    Lower plane culling to cut down on the number of them falling out of the sky

--- Curiosity/Curiosity.Core.Server/Managers/GarageManager.cs
+++ Curiosity/Curiosity.Core.Server/Managers/GarageManager.cs
@@ -217,7 +217,7 @@ namespace Curiosity.Core.Server.Managers
                             if (curiosityUser.PersonalPlane > 0)
                                 EntityManager.GetModule().NetworkDeleteEntity(curiosityUser.PersonalPlane);
 
-                            cullingRange = 5000f;
+                            cullingRange = 1000f;
                             player.State.Set(StateBagKey.VEH_PLANE_NETWORK_ID, vehicle.NetworkId, true);
                             break;
                         case SpawnType.Helicopter:
