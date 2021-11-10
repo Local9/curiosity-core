8c97b033bf44f445d9ebf28226b151f3bed5712b
Antony Cook <kinglo0py@gmail.com>
2021-11-10 21:36:22 +0000

    added server config resource

---
+++ Curiosity/Curiosity.Core.Server/Environment/ServerConfig.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Curiosity.Core.Server.Environment
+{
+    public class Tip
+    {
+        [DataMember(Name = "header")]
+        public string Header { get; set; }
+
+        [DataMember(Name = "content")]
+        public string Content { get; set; }
+    }
+
+    public class ServerConfig
+    {
+        [DataMember(Name = "tips")]
+        public List<Tip> Tips { get; set; }
+
+        [DataMember(Name = "loadingImages")]
+        public List<string> LoadingImages { get; set; }
+    }
+}
