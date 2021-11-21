956d44baf8a4054b7c4e0e7b52c3c28a5f67af6d
local9 <apjcookie1@hotmail.com>
2021-11-21 01:56:57 +0000

    Removed siren states from vehicles

--- Curiosity/Curiosity.Police.Client/Managers/GameEvents/GameEventPlayerKilledPlayerManager.cs
+++ Curiosity/Curiosity.Police.Client/Managers/GameEvents/GameEventPlayerKilledPlayerManager.cs
@@ -17,20 +17,32 @@ namespace Curiosity.Police.Client.Managers.GameEvents
 
         private void GameEventManager_OnPlayerKillPlayer(Player attacker, Player victim, bool isMeleeDamage, uint weaponInfoHash, int damageTypeFlag)
         {
-            if (attacker != Game.Player) return; // I don't care about others
+            if (attacker != Game.Player) return; // I don't care about others, events would be too much
             StateBag victimStateBag = victim.State;
+            int playerServerId = attacker.ServerId;
+            int victimServerId = victim.ServerId;
 
-            bool isVictimWanted = victimStateBag.Get(StateBagKey.PLAYER_WANTED) ?? false;
+            bool isVictimWanted = victimStateBag.Get(StateBagKey.PLAYER_IS_WANTED) ?? false;
 
             // if I'm an officer, I cannot get punished if the victim is flagged
             if (JobManager.IsOfficer && isVictimWanted)
             {
-
+                // check server side
+                return;
             }
 
             if (JobManager.IsOfficer && !isVictimWanted)
             {
+                // punish if a person who is not wanted has been shot and killed
+                // check server side
+                return;
+            }
+
+            if (!JobManager.IsOfficer)
+            {
                 // punish
+                // check server side
+                return;
             }
         }
     }
