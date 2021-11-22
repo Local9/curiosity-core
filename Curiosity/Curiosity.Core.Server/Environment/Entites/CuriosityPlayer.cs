3627058bf1cf422a15a5ca6d5b880df9cb3edc26
local9 <apjcookie1@hotmail.com>
2021-11-22 13:16:00 +0000

    updated entity

--- Curiosity/Curiosity.Core.Server/Environment/Entites/CuriosityPlayer.cs
+++ Curiosity/Curiosity.Core.Server/Environment/Entites/CuriosityPlayer.cs
@@ -1,26 +1,36 @@
-﻿using Curiosity.Systems.Library.Enums;
-using Curiosity.Systems.Library.Models;
+﻿using CitizenFX.Core;
+using Curiosity.Core.Server.Diagnostics;
 using Newtonsoft.Json;
 using System;
+using static CitizenFX.Core.Native.API;
 
 namespace Curiosity.Core.Server.Environment.Entities
 {
     public class CuriosityPlayer
     {
-        [JsonIgnore] public CuriosityUser User { get; set; }
-        [JsonIgnore] public CuriosityEntity Entity { get; set; }
-        [JsonIgnore] public CuriosityCharacter Character { get; set; }
-        public ulong DiscordId { get; set; }
-        public int Handle { get; set; }
-        public string Name { get; set; }
-        public DateTime Joined { get { return User.DateCreated; } }
-        public Role Role { get { return User.Role; } }
-        public string RoleDescription { get { return $"{User.Role}"; } }
+        [JsonIgnore] public Player Player;
+        [JsonIgnore] int StateBagHandler;
 
-        public CuriosityPlayer(ulong discordId, CuriosityEntity entity)
+        public bool IsPassive = false;
+
+        public CuriosityPlayer(Player player)
         {
-            DiscordId = discordId;
-            Entity = entity;
+            Player = player;
+            StateBagHandler = AddStateBagChangeHandler(string.Empty, $"player:{player.Handle}", new Action<string, string, dynamic, int, bool>(OnStateChange));
+        }
+
+        private void OnStateChange(string key, string bag, dynamic bagValue, int reserved, bool replicated)
+        {
+            if (PluginManager.IsDebugging)
+            {
+                string msg = $"OnStateChange" +
+                    $"key: {key}" +
+                    $"bag: {bag}" +
+                    $"reserved: {reserved}" +
+                    $"replicated: {reserved}" +
+                    $"bagValue: {JsonConvert.SerializeObject(bagValue)}";
+                Logger.Debug(msg);
+            }
         }
     }
 }
