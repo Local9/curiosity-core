4d35fd20b61f699b282fc71d60f626d36c26645b
local9 <apjcookie1@hotmail.com>
2021-11-23 13:13:12 +0000

    toggle big map when in vehicle

---
+++ Curiosity/Curiosity.Core.Client/Managers/UI/ControlManager.cs
@@ -0,0 +1,31 @@
+﻿using CitizenFX.Core;
+using static CitizenFX.Core.Native.API;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Curiosity.Core.Client.Managers.UI
+{
+    public class ControlManager : Manager<ControlManager>
+    {
+        public bool CanScreenInteract => (!Game.IsPaused && !IsPauseMenuRestarting() && IsScreenFadedIn() && !IsPlayerSwitchInProgress() && Cache.Character.MarkedAsRegistered);
+
+        public override void Begin()
+        {
+            
+        }
+
+        [TickHandler(SessionWait = true)]
+        private async Task OnControlsTick()
+        {
+            if (Game.IsControlPressed(0, Control.MultiplayerInfo) && CanScreenInteract)
+            {
+                SetRadarBigmapEnabled(true, false);
+                await BaseScript.Delay(10000);
+                SetRadarBigmapEnabled(false, false);
+            }
+        }
+    }
+}
