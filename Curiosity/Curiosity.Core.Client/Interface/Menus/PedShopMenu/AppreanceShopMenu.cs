f3fe7b242605de56ca5c1cde31f03ea6cadcde42
local9 <apjcookie1@hotmail.com>
2021-12-06 23:43:09 +0000

    intial new menu

---
+++ Curiosity/Curiosity.Core.Client/Interface/Menus/PedShopMenu/AppreanceShopMenu.cs
@@ -0,0 +1,23 @@
+﻿using NativeUI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Curiosity.Core.Client.Interface.Menus.PedShopMenu
+{
+    internal class AppreanceShopMenu
+    {
+        UIMenu menu;
+
+        public UIMenu CreateMenu(UIMenu m)
+        {
+            menu = m;
+
+
+
+            return menu;
+        }
+    }
+}
