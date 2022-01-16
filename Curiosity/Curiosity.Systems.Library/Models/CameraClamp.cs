57d9e18dafaf903ba2f3a8a48b7ca1aa92dfa725
local9 <apjcookie1@hotmail.com>
2022-01-16 20:49:55 +0000

    Added CameraClamp

---
+++ Curiosity/Curiosity.Systems.Library/Models/CameraClamp.cs
@@ -0,0 +1,34 @@
+﻿namespace Curiosity.Systems.Library.Models
+{
+    public class CameraClamp
+    {
+        private float _leftHorizontalValue;
+        private float _maxVerticalValue;
+        private float _minVerticalValue;
+        private float _rightHorizontalValue;
+
+        public float LeftHorizontalValue
+        {
+            get => this._leftHorizontalValue;
+            set => this._leftHorizontalValue = value;
+        }
+
+        public float MaxVerticalValue
+        {
+            get => this._maxVerticalValue;
+            set => this._maxVerticalValue = value;
+        }
+
+        public float MinVerticalValue
+        {
+            get => this._minVerticalValue;
+            set => this._minVerticalValue = value;
+        }
+
+        public float RightHorizontalValue
+        {
+            get => this._rightHorizontalValue;
+            set => this._rightHorizontalValue = value;
+        }
+    }
+}
