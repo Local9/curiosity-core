e388651e812832764ffb62b8a19f3a71b52c0069
local9 <apjcookie1@hotmail.com>
2022-01-16 20:47:43 +0000

    Added eCameraPosition

---
+++ Curiosity/Curiosity.Systems.Library/Enums/eCameraPosition.cs
@@ -0,0 +1,28 @@
+﻿namespace Curiosity.Systems.Library.Enums
+{
+    public enum eCameraPosition
+    {
+        Car,
+        Interior,
+        Engine,
+        RearEngine,
+        Trunk,
+        FrontBumper,
+        RearBumper,
+        Grille,
+        Tank,
+        Plaque,
+        BackPlate,
+        FrontPlate,
+        Wheels,
+        Hood,
+        RearWindscreen,
+        BikeExhaust,
+        FrontMuguard,
+        RearMuguard,
+        RearHood,
+        FrontTrunk,
+        Boost,
+        Exhaust
+    }
+}
