89082a7093d3e43bf9e4b85c5a4119b440492d25
local9 <apjcookie1@hotmail.com>
2022-01-21 18:11:19 +0000

    Added rcore events

---
+++ Curiosity/Curiosity.Core.Server/Managers/Thirdparty/ThirdparyRCoreManager.cs
@@ -0,0 +1,28 @@
+﻿using CitizenFX.Core;
+using Curiosity.Core.Server.Diagnostics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Curiosity.Core.Server.Managers.Thirdparty
+{
+    public class ThirdparyRCoreManager : Manager<ThirdparyRCoreManager>
+    {
+        public override void Begin()
+        {
+            Instance.EventRegistry.Add("rcore_races:giveMoney", new Action<int, int, CallbackDelegate>((source, amt, cb) =>
+            {
+                Logger.Info($"rcore_races:giveMoney: PSID: {source} / AMT: {amt}");
+                cb.Invoke(true);
+            }));
+
+            Instance.EventRegistry.Add("rcore_races:takeMoney", new Action<int, int, CallbackDelegate>((source, amt, cb) =>
+            {
+                Logger.Info($"rcore_races:giveMoney: PSID: {source} / AMT: {amt}");
+                cb.Invoke(true);
+            }));
+        }
+    }
+}
