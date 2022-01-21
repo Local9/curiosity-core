2f37e16c7656280e2d7b24073e781f097a3174b4
local9 <apjcookie1@hotmail.com>
2022-01-21 22:49:47 +0000

    fix betting amount

--- Curiosity/Curiosity.Core.Client/Interface/Menus/SubMenu/CustomRaceMenu.cs
+++ Curiosity/Curiosity.Core.Client/Interface/Menus/SubMenu/CustomRaceMenu.cs
@@ -10,29 +10,76 @@ namespace Curiosity.Core.Client.Interface.Menus.SubMenu
         private UIMenu menu;
 
         UIMenuListItem miCreateRace;
+        UIMenuListItem miRaceBet;
         List<dynamic> raceList = new List<dynamic>()
         {
-            new { label = "Short", minimalBet = 500 },
-            new { label = "Normal", minimalBet = 1500 },
-            new { label = "Long", minimalBet = 4500 },
+            new { label = "Short", minimalBet = 500, maxBet = 5000 },
+            new { label = "Normal", minimalBet = 1500, maxBet = 15000 },
+            new { label = "Long", minimalBet = 4500, maxBet = 100000 },
         };
+
+        List<dynamic> betAmount = new List<dynamic>()
+        {
+            500,
+            1000,
+            1500,
+            2000,
+            2500,
+            3000,
+            3500,
+            4000,
+            4500,
+            5000,
+            6000,
+            7000,
+            8000,
+            9000,
+            10000,
+            12500,
+            15000,
+            25000,
+            50000,
+            100000,
+        };
+
+        int betAmountSelected = 1500;
 
         public UIMenu CreateMenu(UIMenu m)
         {
             menu = m;
 
-            miCreateRace = new UIMenuListItem("Create Normal Race", raceList.Select(x => x.label).ToList(), 1);
+            miRaceBet = new UIMenuListItem("Race Bet ($)", betAmount.Select(x => x).ToList(), 3);
+            menu.AddItem(miRaceBet);
+
+            miCreateRace = new UIMenuListItem("Create Race", raceList.Select(x => x.label).ToList(), 1);
             menu.AddItem(miCreateRace);
 
             menu.OnListSelect += Menu_OnListSelect;
+            menu.OnListChange += Menu_OnListChange;
 
             return menu;
         }
 
+        private void Menu_OnListChange(UIMenu sender, UIMenuListItem listItem, int newIndex)
+        {
+            if (listItem == miRaceBet)
+                betAmountSelected = betAmount[newIndex];
+        }
+
         private void Menu_OnListSelect(UIMenu sender, UIMenuListItem listItem, int newIndex)
         {
-            dynamic item = raceList[newIndex];
-            ExecuteCommand($"createrace {item.label} {item.minimalBet}");
+            if (listItem == miCreateRace) {
+                dynamic item = raceList[newIndex];
+
+                if (item.minimalBet >= betAmountSelected && betAmountSelected <= item.maxBet)
+                {
+                    ExecuteCommand($"createrace {item.label} {betAmountSelected}");
+                }
+                else
+                {
+                    Notify.Alert($"Bet must be between ${item.minimalBet:C0} & ${item.maxBet:C0}.");
+                }
+            }
         }
     }
 }
