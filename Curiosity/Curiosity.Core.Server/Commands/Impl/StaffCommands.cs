8f716c6958f4d0512148c9f052958c601e32d16e
local9 <apjcookie1@hotmail.com>
2022-02-07 21:44:32 +0000

    add server command to set player wanted

--- Curiosity/Curiosity.Core.Server/Commands/Impl/StaffCommands.cs
+++ Curiosity/Curiosity.Core.Server/Commands/Impl/StaffCommands.cs
@@ -48,6 +48,32 @@ namespace Curiosity.Core.Server.Commands.Impl
             }
         }
 
+        [CommandInfo(new[] { "wanted" })]
+        public class PlayerWanted : ICommand
+        {
+            public void On(CuriosityUser user, Player player, List<string> arguments)
+            {
+                if (arguments.Count == 0)
+                {
+                    ChatManager.OnChatMessage(player, $"Missing argument.");
+                    return;
+                }
+
+                string arg = arguments.ElementAt(0);
+                string wanted = arguments.ElementAt(1);
+                if (!int.TryParse(arg, out int worldId))
+                {
+                    ChatManager.OnChatMessage(player, $"Argument is not a valid number.");
+                    return;
+                }
+
+                bool isWanted = wanted == "wanted";
+
+                Player p = PluginManager.PlayersList[worldId];
+                p.State.Set(StateBagKey.PLAYER_POLICE_WANTED, isWanted, true);
+            }
+        }
+
         [CommandInfo(new[] { "world" })]
         public class PlayerWorld : ICommand
         {
