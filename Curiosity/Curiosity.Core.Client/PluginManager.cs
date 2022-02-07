b3810fe0eac017a9ea2564f62b1c2c2373aa0ef0
local9 <apjcookie1@hotmail.com>
2022-02-07 23:29:06 +0000

    Use current Native name

--- Curiosity/Curiosity.Core.Client/PluginManager.cs
+++ Curiosity/Curiosity.Core.Client/PluginManager.cs
@@ -227,7 +227,7 @@ namespace Curiosity.Core.Client
             // Screen.Hud.HideComponentThisFrame(HudComponent.WeaponWheel);
 
             int vehicleWeapon = -1553120962;
-            API.N_0x4757f00bc6323cfe((uint)vehicleWeapon, 0.0f);
+            API.SetWeaponDamageModifierThisFrame((uint)vehicleWeapon, 0.0f);
 
             Screen.Hud.HideComponentThisFrame(HudComponent.Cash);
             Screen.Hud.HideComponentThisFrame(HudComponent.CashChange);
