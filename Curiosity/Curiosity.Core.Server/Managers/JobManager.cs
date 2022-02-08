78afe16a19c7dbb3569d00e0fcdfb444706a8144
local9 <apjcookie1@hotmail.com>
2022-02-08 11:18:42 +0000

    Job manager, removed all job methods

--- Curiosity/Curiosity.Core.Server/Managers/JobManager.cs
+++ Curiosity/Curiosity.Core.Server/Managers/JobManager.cs
@@ -10,64 +10,8 @@ namespace Curiosity.Core.Server.Managers
 {
     public class JobManager : Manager<JobManager>
     {
-        private const string JOB_UNEMPLOYED = "job:unemployed";
-        private const string JOB_POLICE_DUTY = "job:police:duty";
-        private const string JOB_POLICE_ARREST = "job:police:arrest";
-        private LocationsConfigManager config;
-
         public override void Begin()
         {
-            config = LocationsConfigManager.GetModule();
-
-            EventSystem.Attach(JOB_UNEMPLOYED, new AsyncEventCallback(async metadata =>
-            {
-                if (!PluginManager.ActiveUsers.ContainsKey(metadata.Sender))
-                    return null;
-
-                CuriosityUser user = PluginManager.ActiveUsers[metadata.Sender];
-
-                Logger.Debug($"'{user.LatestName}' is now a {user.CurrentJob}");
-
-                await BaseScript.Delay(5000);
-
-                return null;
-            }));
-
-            EventSystem.Attach(JOB_POLICE_DUTY, new AsyncEventCallback(async metadata =>
-            {
-                if (!PluginManager.ActiveUsers.ContainsKey(metadata.Sender))
-                    return null;
-
-                CuriosityUser user = PluginManager.ActiveUsers[metadata.Sender];
-
-                Logger.Debug($"'{user.LatestName}' is now a {user.CurrentJob}");
-
-                await BaseScript.Delay(5000);
-
-                return null;
-            }));
-
-            EventSystem.Attach(JOB_POLICE_ARREST, new AsyncEventCallback(async metadata =>
-            {
-                if (!PluginManager.ActiveUsers.ContainsKey(metadata.Sender))
-                    return null;
-
-                CuriosityUser user = PluginManager.ActiveUsers[metadata.Sender];
-
-                Vector3 position = Vector3.Zero;
-                position.X = metadata.Find<float>(0);
-                position.Y = metadata.Find<float>(1);
-                position.Z = metadata.Find<float>(2);
-
-                bool result = config.IsNearEventLocation(position, JOB_POLICE_ARREST);
-
-                user.Send(JOB_POLICE_ARREST);
-
-                await BaseScript.Delay(5000);
-
-                return null;
-            }));
-
             Instance.ExportDictionary.Add("SetUserJob", new Func<string, string, bool>(
                 (playerHandle, job) =>
                 {
