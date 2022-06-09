e4188293c919e3ffdd094f8920bba6ef4f389abe
local9 <apjcookie1@hotmail.com>
2022-06-09 20:15:48 +0100

    Base64 Extension

---
+++ Curiosity/Curiosity.Framework.Shared/Extensions/Base64Extensions.cs
@@ -0,0 +1,17 @@
+﻿using System.Text;
+
+namespace Curiosity.Framework.Shared.Extensions
+{
+    public static class Base64Extensions
+    {
+        public static string ToBase64(this string value)
+        {
+            return Convert.ToBase64String(Encoding.UTF8.GetBytes(value));
+        }
+
+        public static string FromBase64(this string serialized)
+        {
+            return Encoding.UTF8.GetString(Convert.FromBase64String(serialized));
+        }
+    }
+}
