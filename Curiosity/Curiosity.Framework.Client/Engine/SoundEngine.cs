5666396b61cc612aeb09c85828f25638fc4e32d2
local9 <apjcookie1@hotmail.com>
2022-07-03 20:32:57 +0100

    Added SoundEngine

---
+++ Curiosity/Curiosity.Framework.Client/Engine/SoundEngine.cs
@@ -0,0 +1,36 @@
+﻿namespace Curiosity.Framework.Client.Engine
+{
+    internal class SoundEngine
+    {
+        public bool IsDisabled { get; protected set; }
+
+        public void Disable()
+        {
+            IsDisabled = true;
+
+            Play("MP_LEADERBOARD_SCENE");
+        }
+
+        public void Enable()
+        {
+            IsDisabled = false;
+
+            Stop("MP_LEADERBOARD_SCENE");
+        }
+
+        public void Play(string audioScene)
+        {
+            StartAudioScene(audioScene);
+        }
+
+        public void PlayFrontend(string audioName, string audioRef)
+        {
+            PlaySoundFrontend(-1, audioName, audioRef, false);
+        }
+
+        public void Stop(string audioScene)
+        {
+            StopAudioScene(audioScene);
+        }
+    }
+}
