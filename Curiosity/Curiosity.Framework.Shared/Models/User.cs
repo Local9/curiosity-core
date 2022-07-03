b29f37ff2e5f5b0cff12bfdcb190a7042f12df26
local9 <apjcookie1@hotmail.com>
2022-07-03 20:33:51 +0100

    Added methods to get User and Characters

--- Curiosity/Curiosity.Framework.Shared/Models/User.cs
+++ Curiosity/Curiosity.Framework.Shared/Models/User.cs
@@ -1,20 +1,32 @@
-﻿using System.Collections.Generic;
+﻿#if CLIENT
+using Curiosity.Framework.Client.Engine;
+#endif
+
+#if SERVER
+using Curiosity.Framework.Server;
+using Curiosity.Framework.Server.Database;
+using Dapper;
+#endif
+
 using System.ComponentModel;
 
 namespace Curiosity.Framework.Shared.Models
 {
     public class User
     {
-        #region FIELDS
+#region FIELDS
+        [JsonIgnore]
         [Description("userId")]
         public int UserID { get; private set; }
 
         [Description("username")]
         public string Username { get; private set; }
 
+        [JsonIgnore]
         [Description("license")]
         public string License { get; private set; }
 
+        [JsonIgnore]
         [Description("discordId")]
         public ulong DiscordId { get; private set; }
 
@@ -33,8 +45,41 @@ namespace Curiosity.Framework.Shared.Models
         [Description("isPassive")]
         public int IsPassive { get; private set; }
 
-        #endregion
+#endregion
 
         public List<Character> Characters { get; private set; } = new();
+
+#if CLIENT
+        internal SoundEngine Sound => new SoundEngine();
+#endif
+
+#if SERVER
+        const string SQL_GET_USER = "call spGetUser(@pUsername, @pDiscordId);";
+        const string SQL_GET_CHARACTERS = "call spGetCharacter(@pDiscordID, @pServerID);";
+
+        public async Task<User> GetUserAsync(string username, string discordId, bool withCharacters = false)
+        {
+            DynamicParameters dynamicParameters = new DynamicParameters();
+            dynamicParameters.Add("pUsername", username);
+            dynamicParameters.Add("pDiscordId", discordId);
+            User user = await DapperDatabase<User>.GetSingleAsync(SQL_GET_USER, dynamicParameters);
+            
+            await Common.MoveToMainThread();
+
+            if (withCharacters)
+                await GetCharactersAsync();
+
+            return user;
+        }
+
+        public async Task GetCharactersAsync()
+        {
+            DynamicParameters dynamicParameters = new DynamicParameters();
+            dynamicParameters.Add("pDiscordId", DiscordId);
+            dynamicParameters.Add("pServerID", PluginManager.ServerID);
+            Characters = await DapperDatabase<Character>.GetListAsync(SQL_GET_CHARACTERS, dynamicParameters);
+            await Common.MoveToMainThread();
+        }
+#endif
     }
 }
