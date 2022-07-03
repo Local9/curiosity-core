a8ab308a480526e643e83e6a64f31442f8d27861
local9 <apjcookie1@hotmail.com>
2022-07-03 20:33:21 +0100

    Disable sound on init

--- Curiosity/Curiosity.Framework.Client/Scripts/CharacterScript.cs
+++ Curiosity/Curiosity.Framework.Client/Scripts/CharacterScript.cs
@@ -1,7 +1,14 @@
-﻿namespace Curiosity.Framework.Client.Scripts
+﻿using Curiosity.Framework.Shared.Models;
+
+namespace Curiosity.Framework.Client.Scripts
 {
     internal class CharacterScript
     {
+        static User _user = new();
 
+        public static void Init()
+        {
+            _user.Sound.Disable();
+        }
     }
 }
