c0aaaad115ac990c70f5fa7d4da6a6568ef43df8
local9 <apjcookie1@hotmail.com>
2022-07-27 23:15:04 +0100

    added door class

---
+++ Curiosity/Curiosity.Core.Client/Managers/GameWorld/Properties/Models/Door.cs
@@ -0,0 +1,26 @@
+﻿namespace Curiosity.Core.Client.Managers.GameWorld.Properties.Models
+{
+    internal class Door
+    {
+        private int _hash;
+        public string Model { get; set; }
+        public Vector3 Position { get; set; }
+
+        public Door(string model, Vector3 position)
+        {
+            Model = model;
+            Position = position;
+            _hash = GetHashKey(model);
+        }
+
+        public void Lock()
+        {
+            DoorControl((uint)_hash, Position.X, Position.Y, Position.Z, true, 0f, 50f, 0f);
+        }
+
+        public void Unlock()
+        {
+            DoorControl((uint)_hash, Position.X, Position.Y, Position.Z, false, 0f, 50f, 0f);
+        }
+    }
+}
