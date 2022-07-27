17b4cb7b6778b5f77b9c27a71cc5e1c03e7a599a
local9 <apjcookie1@hotmail.com>
2022-07-27 23:15:17 +0100

    added more variables to the building class

--- Curiosity/Curiosity.Core.Client/Managers/GameWorld/Properties/Models/Building.cs
+++ Curiosity/Curiosity.Core.Client/Managers/GameWorld/Properties/Models/Building.cs
@@ -1,4 +1,6 @@
-﻿namespace Curiosity.Core.Client.Managers.GameWorld.Properties.Models
+﻿using Curiosity.Core.Client.Managers.GameWorld.Properties.Enums;
+
+namespace Curiosity.Core.Client.Managers.GameWorld.Properties.Models
 {
     internal class Building
     {
@@ -13,5 +15,14 @@
         public BuildingCamera EnteranceCamera4 { get; set; }
         public eGarageType GarageType { get; set; }
         public eBuildingType BuildingType { get; set; }
+        public List<Apartment> Apartments { get; set; }
+        public string[] HideObjects { get; set; }
+        public Vector3 SaleSignPosition { get; set; }
+        public eFrontDoor FrontDoor { get; set; }
+        public Door Door1 { get; set; }
+        public Door Door2 { get; set; }
+        public Door Door3 { get; set; }
+        public Blip BuildingBlip { get; set; }
+        public Blip SaleSignBlip { get; set; }
     }
 }
