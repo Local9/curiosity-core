c3c50ac294d2486fa3dcbad6a7c57042e5783a3a
local9 <apjcookie1@hotmail.com>
2022-07-27 23:08:38 +0100

    added building

--- Curiosity/Curiosity.Core.Client/Managers/GameWorld/Properties/Models/Building.cs
+++ Curiosity/Curiosity.Core.Client/Managers/GameWorld/Properties/Models/Building.cs
@@ -6,5 +6,12 @@
         public Quaternion Enterance { get; set; }
         public Quaternion Exit { get; set; }
         public Quaternion Lobby { get; set; }
+        public BuildingCamera Camera { get; set; }
+        public BuildingCamera EnteranceCamera1 { get; set; }
+        public BuildingCamera EnteranceCamera2 { get; set; }
+        public BuildingCamera EnteranceCamera3 { get; set; }
+        public BuildingCamera EnteranceCamera4 { get; set; }
+        public eGarageType GarageType { get; set; }
+        public eBuildingType BuildingType { get; set; }
     }
 }
