3f1257d3164945561f846c4efed1fa883dded0fa
local9 <apjcookie1@hotmail.com>
2022-07-27 23:08:33 +0100

    added building camera

---
+++ Curiosity/Curiosity.Core.Client/Managers/GameWorld/Properties/Models/BuildingCamera.cs
@@ -0,0 +1,21 @@
+﻿namespace Curiosity.Core.Client.Managers.GameWorld.Properties.Models
+{
+    internal class BuildingCamera
+    {
+        public Vector3 Position;
+        public Vector3 Rotation;
+        public float FieldOfView;
+
+        public BuildingCamera(Vector3 position, Vector3 rotation, float fieldOfView)
+        {
+            Position = position;
+            Rotation = rotation;
+            FieldOfView = fieldOfView;
+        }
+
+        public BuildingCamera Zero()
+        {
+            return new BuildingCamera(Vector3.Zero, Vector3.Zero, 0f);
+        }
+    }
+}
