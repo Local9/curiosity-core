bbdb35822306c8c9318cf421fe764de2cdab8c2d
local9 <apjcookie1@hotmail.com>
2022-07-30 00:52:01 +0100

    initial test work

--- Curiosity/Curiosity.Core.Client/Managers/GameWorld/Properties/PropertyManager.cs
+++ Curiosity/Curiosity.Core.Client/Managers/GameWorld/Properties/PropertyManager.cs
@@ -1,4 +1,5 @@
-﻿using Curiosity.Core.Client.Managers.GameWorld.Properties.Enums;
+﻿using Curiosity.Core.Client.Extensions;
+using Curiosity.Core.Client.Managers.GameWorld.Properties.Enums;
 using Curiosity.Core.Client.Managers.GameWorld.Properties.Models;
 
 namespace Curiosity.Core.Client.Managers.GameWorld.Properties
@@ -12,6 +13,22 @@ namespace Curiosity.Core.Client.Managers.GameWorld.Properties
         {
             await Session.Loading();
 
+            Apartment _ThreeAltaStreet10 = new();
+
+            _ThreeAltaStreet10.Id = 5;
+            _ThreeAltaStreet10.Name = "MP_PROP_5";
+            _ThreeAltaStreet10.Description = "MP_PROP_5DES";
+            _ThreeAltaStreet10.Price = 217000;
+            _ThreeAltaStreet10.Bed = new Quaternion(-260.0051f, -949.0003f, 70.02404f, 0f);
+            _ThreeAltaStreet10.DoorPosition = new Quaternion(-264.7441f, -969.7109f, 76.23132f, 0f);
+            _ThreeAltaStreet10.Door = new Door("hei_v_ilev_fh_heistdoor2", new Vector3(-264.6448f, -967.7161f, 77.34515f));
+            _ThreeAltaStreet10.Enterance = new Quaternion(-262.9719f, -965.5146f, 76.23132f, 0f);
+            _ThreeAltaStreet10.Exit = new Quaternion(-264.0814f, -967.5364f, 76.23132f, 0f);
+            _ThreeAltaStreet10.Wardrobe = new Quaternion(-265.4738f, -947.6708f, 70.03869f, 160.6728f);
+            _ThreeAltaStreet10.GarageFile = "3_alta_st_apt_10";
+            _ThreeAltaStreet10.EnteranceCamera = new BuildingCamera(new Vector3(-262.6027f, -966.4926f, 77.74511f), new Vector3(0.1007454f, -0.000003034903f, 134.0962f), 50.0f);
+            _ThreeAltaStreet10.ExitCamera = new BuildingCamera(new Vector3(-263.5748f, -969.6506f, 77.6988f), new Vector3(-0.05672785f, -0.0000001992695f, 10.8675f), 50.0f);
+
             Building _ThreeAltaStreet = new();
             _ThreeAltaStreet.Name = "3 Alta St";
             _ThreeAltaStreet.Enterance = new Quaternion(-259.8061f, -969.4397f, 30.21999f, 70.91596f);
@@ -20,8 +37,8 @@ namespace Curiosity.Core.Client.Managers.GameWorld.Properties
             _ThreeAltaStreet.Camera = new BuildingCamera(new Vector3(-215.2378f, -1071.639f, 32.85828f), new Vector3(22.62831f, 0f, 26.93762f), 50.0f);
             _ThreeAltaStreet.EnteranceCamera1 = new BuildingCamera(new Vector3(-261.2893f, -985.9326f, 34.31419f), new Vector3(-15.94485f, 0, 0.3253375f), 50.0f);
             _ThreeAltaStreet.EnteranceCamera2 = new BuildingCamera(new Vector3(-261.2893f, -985.9326f, 34.31419f), new Vector3(65.94485f, 0, 0.3253375f), 50.0f);
-            _ThreeAltaStreet.EnteranceCamera3 = new BuildingCamera(new Vector3(-288.8877f, -994.0138f, 24.12381f), new Vector3(-3.488037f, -0.0000001069198f, -100.3268f), 50.0f); // Garage Camera
-            _ThreeAltaStreet.EnteranceCamera4 = new BuildingCamera(new Vector3(-298.3831f, -990.7642f, 24.12381f), new Vector3(-3.879195f, -0.00000001337085f, -87.55238f), 50.0f); // Garage Camera
+            _ThreeAltaStreet.GarageCamera1 = new BuildingCamera(new Vector3(-288.8877f, -994.0138f, 24.12381f), new Vector3(-3.488037f, -0.0000001069198f, -100.3268f), 50.0f); // Garage Camera
+            _ThreeAltaStreet.GarageCamera2 = new BuildingCamera(new Vector3(-298.3831f, -990.7642f, 24.12381f), new Vector3(-3.879195f, -0.00000001337085f, -87.55238f), 50.0f); // Garage Camera
             _ThreeAltaStreet.BuildingType = eBuildingType.Apartment;
             _ThreeAltaStreet.ExteriorIndex = 5;
             _ThreeAltaStreet.FrontDoor = eFrontDoor.DoubleDoors;
@@ -37,36 +54,45 @@ namespace Curiosity.Core.Client.Managers.GameWorld.Properties
             _ThreeAltaStreet.Garage.GarageType = eGarageType.Ten;
             _ThreeAltaStreet.Garage.Door = eFrontDoor.StandardDoor;
             _ThreeAltaStreet.Garage.Waypoint = new Quaternion(-292.5203f, -991.2855f, 23.47978f, 250.2265f);
+            _ThreeAltaStreet.Apartments.Add(_ThreeAltaStreet10);
+
             _buildings.Add(_ThreeAltaStreet); // Move all of this into a JSON file
 
             foreach(Building building in _buildings)
             {
-                building.CreateBuilding();
+                    building.CreateBuilding();
             }
         }
 
-        int currentLocation = 5;
+        bool _transition;
 
         // [TickHandler]
-        private async Task OnPropertyManager()
+        private async Task OnBuildingTick()
         {
-            if (Game.IsControlJustPressed(0, Control.FrontendDown))
-                --currentLocation;
+            try
+            {
+                foreach (Building building in _buildings)
+                {
+                    if (Game.PlayerPed.IsInRangeOf(building.Lobby.AsVector(), 2f) && !_transition)
+                    {
+                        _transition = true;
+                        Audio.PlaySoundAt(Game.PlayerPed.Position, "DOOR_BUZZ", "MP_PLAYER_APARTMENT");
+                        await building.PlayEnterApartmentCamera(3000, true, true, CameraShake.Hand, 0.4f);
+                        Apartment apartment = building.Apartments[0];
+                        apartment.SetInteriorActive();
+                        Game.PlayerPed.Position = apartment.Enterance.AsVector();
+                        // DOOR SCRIPT
+                        await apartment.PlayEnteranceCutscene();
+                        World.DestroyAllCameras();
+                        World.RenderingCamera = null;
+                        _transition = false;
+                    }
+                }
+            }
+            catch(Exception ex)
+            {
 
-            if (Game.IsControlJustPressed(0, Control.FrontendUp))
-                ++currentLocation;
-
-            if (currentLocation < 0)
-                currentLocation = 114;
-
-            if (currentLocation > 114)
-                currentLocation = 0;
-
-            DisableExterior(currentLocation);
-
-            int currentInterior = GetInteriorFromEntity(Game.PlayerPed.Handle);
-
-            // Screen.DisplayHelpTextThisFrame($"Current Location: {currentLocation}~n~Int: {currentInterior}");
+            }
         }
 
         private void DisableExterior(int location)
