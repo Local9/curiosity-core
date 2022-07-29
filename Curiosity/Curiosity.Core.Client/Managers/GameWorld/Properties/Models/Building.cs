c30ea0b892fb8ae1520b7c35a45faeda3974503b
local9 <apjcookie1@hotmail.com>
2022-07-30 00:51:52 +0100

    Add enter and exit animations

--- Curiosity/Curiosity.Core.Client/Managers/GameWorld/Properties/Models/Building.cs
+++ Curiosity/Curiosity.Core.Client/Managers/GameWorld/Properties/Models/Building.cs
@@ -7,6 +7,7 @@ namespace Curiosity.Core.Client.Managers.GameWorld.Properties.Models
     {
         private const BlipColor BLIP_COLOR_BLACK = (BlipColor)40;
         private const BlipColor BLIP_COLOR_WHITE = (BlipColor)4;
+        private bool _hideHud;
 
         public string Name { get; set; }
         public Quaternion Enterance { get; set; }
@@ -15,10 +16,10 @@ namespace Curiosity.Core.Client.Managers.GameWorld.Properties.Models
         public BuildingCamera Camera { get; set; }
         public BuildingCamera EnteranceCamera1 { get; set; }
         public BuildingCamera EnteranceCamera2 { get; set; }
-        public BuildingCamera EnteranceCamera3 { get; set; }
-        public BuildingCamera EnteranceCamera4 { get; set; }
+        public BuildingCamera GarageCamera1 { get; set; }
+        public BuildingCamera GarageCamera2 { get; set; }
         public eBuildingType BuildingType { get; set; }
-        public List<Apartment> Apartments { get; set; }
+        public List<Apartment> Apartments { get; set; } = new();
         public int ExteriorIndex { get; set; }
         public SaleSign SaleSign { get; set; }
         public eFrontDoor FrontDoor { get; set; }
@@ -27,12 +28,14 @@ namespace Curiosity.Core.Client.Managers.GameWorld.Properties.Models
         public Door Door3 { get; set; }
         public Blip BuildingBlip { get; set; }
         public Blip SaleSignBlip { get; set; }
-        public Garage Garage { get;  set; }
+        public Garage Garage { get; set; }
+
+        public bool BuildingSetup { get; set; }
 
         public void CreateBuilding()
         {
             SetupBlip();
-            if (SaleSign is not null) SaleSign.CreateForSaleSign();
+            SaleSign.CreateForSaleSign();
         }
 
         void SetupBlip()
@@ -53,6 +56,70 @@ namespace Curiosity.Core.Client.Managers.GameWorld.Properties.Models
             }
 
             // BuildingBlip.Color = BLIP_COLOR_BLACK;
+        }
+
+        public void ToggleDoors(bool unlock = false)
+        {
+            if (unlock)
+            {
+                switch (FrontDoor)
+                {
+                    case eFrontDoor.DoubleDoors:
+                        Door1.Unlock();
+                        Door2.Unlock();
+                        break;
+                    case eFrontDoor.StandardDoor:
+                        Door1.Unlock();
+                        break;
+                }
+                return;
+            }
+            switch (FrontDoor)
+            {
+                case eFrontDoor.DoubleDoors:
+                    Door1.Lock();
+                    Door2.Lock();
+                    break;
+                case eFrontDoor.StandardDoor:
+                    Door1.Lock();
+                    break;
+            }
+        }
+
+        public async Task PlayEnterApartmentCamera(int duration, bool easePosition, bool easeRotation, CameraShake cameraShake, float cameraShakeAmplitude)
+        {
+            _hideHud = true;
+            ToggleDoors(true);
+            Game.PlayerPed.Task.GoTo(Lobby.AsVector(), true, 7000);
+            Camera scriptCamera = World.CreateCamera(EnteranceCamera1.Position, EnteranceCamera1.Rotation, EnteranceCamera1.FieldOfView);
+            Camera interpCamera = World.CreateCamera(EnteranceCamera2.Position, EnteranceCamera2.Rotation, EnteranceCamera2.FieldOfView);
+            World.RenderingCamera = scriptCamera;
+            scriptCamera.InterpTo(interpCamera, duration, easePosition, easeRotation);
+            World.RenderingCamera = interpCamera;
+            interpCamera.Shake(cameraShake, cameraShakeAmplitude);
+            await BaseScript.Delay(duration);
+            ToggleDoors(false);
+            _hideHud = false;
+        }
+
+        public async Task PlayExitApartmentCamera(int duration, bool easePosition, bool easeRotation, CameraShake cameraShake, float cameraShakeAmplitude)
+        {
+            _hideHud = true;
+            Game.PlayerPed.Position = Lobby.AsVector();
+            Game.PlayerPed.Heading = Exit.W;
+            ToggleDoors(true);
+            Game.PlayerPed.Task.GoTo(Exit.AsVector(), true, 7000);
+            Camera scriptCamera = World.CreateCamera(EnteranceCamera2.Position, EnteranceCamera2.Rotation, EnteranceCamera2.FieldOfView);
+            Camera interpCamera = World.CreateCamera(EnteranceCamera1.Position, EnteranceCamera1.Rotation, EnteranceCamera1.FieldOfView);
+            World.RenderingCamera = scriptCamera;
+            scriptCamera.InterpTo(interpCamera, duration, easePosition, easeRotation);
+            World.RenderingCamera = interpCamera;
+            interpCamera.Shake(cameraShake, cameraShakeAmplitude);
+            await BaseScript.Delay(duration);
+            World.DestroyAllCameras();
+            World.RenderingCamera = null;
+            ToggleDoors(false);
+            _hideHud = false;
         }
     }
 }
