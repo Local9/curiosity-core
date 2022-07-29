5955361f49f38731173d072f0d70420fde074715
local9 <apjcookie1@hotmail.com>
2022-07-30 00:51:13 +0100

    added apartment enum

---
+++ Curiosity/Curiosity.Core.Client/Managers/GameWorld/Properties/Enums/eApartmentType.cs
@@ -0,0 +1,12 @@
+﻿namespace Curiosity.Core.Client.Managers.GameWorld.Properties.Enums
+{
+    internal enum eApartmentType
+    {
+        None, // highend?
+        MediumEnd,
+        LowEnd,
+        IPL,
+        IPLStyle,
+        Other
+    }
+}
