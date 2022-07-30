f0effd22fa86e5dce4b38ffed52fa30ab87c46dc
local9 <apjcookie1@hotmail.com>
2022-07-30 15:57:31 +0100

    Initial tick to show vehicle stats

--- Curiosity/Curiosity.Core.Client/Managers/GameWorld/Properties/PropertyManager.cs
+++ Curiosity/Curiosity.Core.Client/Managers/GameWorld/Properties/PropertyManager.cs
@@ -61,40 +61,78 @@ namespace Curiosity.Core.Client.Managers.GameWorld.Properties
             foreach(Building building in _buildings)
             {
                     building.CreateBuilding();
-
-                
             }
         }
 
         bool _transition;
 
-        // [TickHandler]
-        private async Task OnBuildingTick()
+        private async Task OnShowVehicleStatisticsTickAsync()
         {
             try
             {
-                foreach (Building building in _buildings)
+                Vehicle[] closestVehicles = Game.PlayerPed.GetNearbyVehicles(20f);
+
+                for(int i = 0; i < 10; i++)
                 {
-                    if (Game.PlayerPed.IsInRangeOf(building.Lobby.AsVector(), 2f) && !_transition)
+                    Scaleform scaleform = VehicleExtensions.CarStatScaleform;
+
+                    switch(i)
                     {
-                        _transition = true;
-                        Audio.PlaySoundAt(Game.PlayerPed.Position, "DOOR_BUZZ", "MP_PLAYER_APARTMENT");
-                        await building.PlayEnterApartmentCamera(3000, true, true, CameraShake.Hand, 0.4f);
-                        Apartment apartment = building.Apartments[0];
-                        apartment.SetInteriorActive();
-                        Game.PlayerPed.Position = apartment.Enterance.AsVector();
-                        // DOOR SCRIPT
-                        await apartment.PlayEnteranceCutscene();
-                        World.DestroyAllCameras();
-                        World.RenderingCamera = null;
-                        _transition = false;
+                        case 0:
+                            scaleform = VehicleExtensions.CarStatScaleform;
+                            break;
+                        case 1:
+                            scaleform = VehicleExtensions.CarStatScaleform2;
+                            break;
+                        case 2:
+                            scaleform = VehicleExtensions.CarStatScaleform3;
+                            break;
+                        case 3:
+                            scaleform = VehicleExtensions.CarStatScaleform4;
+                            break;
+                        case 4:
+                            scaleform = VehicleExtensions.CarStatScaleform5;
+                            break;
+                        case 5:
+                            scaleform = VehicleExtensions.CarStatScaleform6;
+                            break;
+                        case 6:
+                            scaleform = VehicleExtensions.CarStatScaleform7;
+                            break;
+                        case 7:
+                            scaleform = VehicleExtensions.CarStatScaleform8;
+                            break;
+                        case 8:
+                            scaleform = VehicleExtensions.CarStatScaleform9;
+                            break;
+                        case 9:
+                            scaleform = VehicleExtensions.CarStatScaleform10;
+                            break;
                     }
+
+                    closestVehicles[i].DrawCarStats(scaleform);
                 }
+
             }
             catch(Exception ex)
             {
 
             }
+        }
+
+        private async Task EnterApartment()
+        {
+            //_transition = true;
+            //Audio.PlaySoundAt(Game.PlayerPed.Position, "DOOR_BUZZ", "MP_PLAYER_APARTMENT");
+            //await building.PlayEnterApartmentCamera(3000, true, true, CameraShake.Hand, 0.4f);
+            //Apartment apartment = building.Apartments[0];
+            //apartment.SetInteriorActive();
+            //Game.PlayerPed.Position = apartment.Enterance.AsVector();
+            //// DOOR SCRIPT
+            //await apartment.PlayEnteranceCutscene();
+            //World.DestroyAllCameras();
+            //World.RenderingCamera = null;
+            //_transition = false;
         }
     }
 }
