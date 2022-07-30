1e3d63cbf51f215b483ee2db51a549c5d1726aa2
local9 <apjcookie1@hotmail.com>
2022-07-30 13:01:33 +0100

    Added vehicle spawn locations

---
+++ Curiosity/Curiosity.Core.Client/Environment/Entities/Models/Vector5.cs
@@ -0,0 +1,14 @@
+﻿namespace Curiosity.Core.Client.Environment.Entities.Models
+{
+    internal class Vector5
+    {
+        public Vector3 Vector3 { get; set; }
+        public Vector2 Vector2 { get; set; }
+
+        public Vector5(Vector3 vector3, Vector2 vector2)
+        {
+            Vector3 = vector3;
+            Vector2 = vector2;
+        }
+    }
+}
---
+++ Curiosity/Curiosity.Core.Client/Managers/GameWorld/Properties/PropertyHelper.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Curiosity.Core.Client.Managers.GameWorld.Properties
+{
+    internal class PropertyHelper
+    {
+        public Scaleform CarStatScaleform = new Scaleform("MP_CAR_STATS_01");
+        public Scaleform CarStatScaleform2 = new Scaleform("MP_CAR_STATS_02");
+        public Scaleform CarStatScaleform3 = new Scaleform("MP_CAR_STATS_03");
+        public Scaleform CarStatScaleform4 = new Scaleform("MP_CAR_STATS_04");
+        public Scaleform CarStatScaleform5 = new Scaleform("MP_CAR_STATS_05");
+        public Scaleform CarStatScaleform6 = new Scaleform("MP_CAR_STATS_06");
+        public Scaleform CarStatScaleform7 = new Scaleform("MP_CAR_STATS_07");
+        public Scaleform CarStatScaleform8 = new Scaleform("MP_CAR_STATS_08");
+        public Scaleform CarStatScaleform9 = new Scaleform("MP_CAR_STATS_09");
+        public Scaleform CarStatScaleform10 = new Scaleform("MP_CAR_STATS_10");
+
+        
+    }
+}
