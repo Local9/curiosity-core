00e7869be4f8540cb46b93497b56c09c93fafbd3
local9 <apjcookie1@hotmail.com>
2022-08-04 01:37:32 +0100

    handle if user is not found

--- Curiosity/Perseverance.Discord.Bot/SlashCommands/UserCommands.cs
+++ Curiosity/Perseverance.Discord.Bot/SlashCommands/UserCommands.cs
@@ -1,24 +1,24 @@
 ﻿using DSharpPlus.Entities;
 using DSharpPlus.SlashCommands;
 using Perseverance.Discord.Bot.Database.Store;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Perseverance.Discord.Bot.SlashCommands
 {
+    [SlashCommandGroup("user", "User related commands.")]
     public class UserCommands : ApplicationCommandModule
     {
         [SlashCommand("whois", "Get information about a user")]
         public async Task PingCommand(InteractionContext ctx, [Option("user", "User to lookup")] DiscordUser user)
         {
             DatabaseUser databaseUser = await Database.Store.DatabaseUser.GetUserAsync(user.Id);
+            string message = $"User '{user.Username}' not found.";
+
+            if (databaseUser is not null)
+                message = $"{databaseUser}";
 
             await ctx.CreateResponseAsync(InteractionResponseType.ChannelMessageWithSource, new DSharpPlus.Entities.DiscordInteractionResponseBuilder()
             {
-                Content = $"{databaseUser}"
+                Content = message
             });
         }
     }
