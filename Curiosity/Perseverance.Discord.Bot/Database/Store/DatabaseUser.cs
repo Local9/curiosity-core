00714f303b6385b0745fee96559099b18d534f50
local9 <apjcookie1@hotmail.com>
2022-08-04 01:19:56 +0100

    initial user class

---
+++ Curiosity/Perseverance.Discord.Bot/Database/Store/DatabaseUser.cs
@@ -0,0 +1,33 @@
+﻿using Dapper;
+using System.ComponentModel;
+
+namespace Perseverance.Discord.Bot.Database.Store
+{
+    internal class DatabaseUser
+    {
+        const string SQL_USER_GET = "select * from curiosity.user u where u.discordId = @pDiscordId;";
+
+        [Description("userId")]
+        public int UserID { get; private set; }
+        
+        [Description("username")]
+        public string Username { get; private set; }
+
+        [Description("discordId")]
+        public ulong DiscordId { get; private set; }
+
+        [Description("dateCreated")]
+        public DateTime DateCreated { get; private set; }
+
+        [Description("lastSeen")]
+        public DateTime LastSeen { get; private set; }
+
+        internal static async Task<DatabaseUser> GetUserAsync(ulong discordId)
+        {
+            DynamicParameters dynamicParameters = new DynamicParameters();
+            dynamicParameters.Add("pDiscordId", discordId);
+            DatabaseUser user = await DapperDatabase<DatabaseUser>.GetSingleAsync(SQL_USER_GET, dynamicParameters);
+            return user;
+        }
+    }
+}
