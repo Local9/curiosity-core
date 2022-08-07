1470740726b82edd14d1e279106fbf9e332f9723
local9 <apjcookie1@hotmail.com>
2022-08-07 15:05:30 +0100

    add user slash command

--- Curiosity/Perseverance.Discord.Bot/SlashCommands/UserCommands.cs
+++ Curiosity/Perseverance.Discord.Bot/SlashCommands/UserCommands.cs
@@ -1,6 +1,7 @@
 ﻿using DSharpPlus.Entities;
 using DSharpPlus.SlashCommands;
 using Perseverance.Discord.Bot.Database.Store;
+using Perseverance.Discord.Bot.Logic;
 
 namespace Perseverance.Discord.Bot.SlashCommands
 {
@@ -20,6 +21,14 @@ namespace Perseverance.Discord.Bot.SlashCommands
             {
                 Content = message
             });
+        }
+
+
+        [SlashCommand("donate", "Checks the users donation status and messages them.")]
+        public async Task DonateCommand(InteractionContext ctx, [Option("user", "User to lookup")] DiscordUser user)
+        {
+            DiscordMember member = await ctx.Guild.GetMemberAsync(user.Id);
+            DiscordMemberLogic.UpdateDonationRole(member);
         }
     }
 }
