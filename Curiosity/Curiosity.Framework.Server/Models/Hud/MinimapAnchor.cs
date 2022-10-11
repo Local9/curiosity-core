c74378efacd6f0286aed98ef0ae705d9b646fe19
local9 <apjcookie1@hotmail.com>
2022-10-11 23:36:12 +0100

    feat(minimap): added anchor class

---
+++ Curiosity/Curiosity.Framework.Server/Models/Hud/MinimapAnchor.cs
@@ -0,0 +1,14 @@
+﻿namespace Curiosity.Framework.Server.Models.Hud
+{
+    public class MinimapAnchor
+    {
+        public float X { get; set; }
+        public double RightX { get; set; }
+        public float Y { get; set; }
+        public double BottomY { get; set; }
+        public float Width { get; set; }
+        public float Height { get; set; }
+        public float UnitX { get; set; }
+        public float UnitY { get; set; }
+    }
+}
