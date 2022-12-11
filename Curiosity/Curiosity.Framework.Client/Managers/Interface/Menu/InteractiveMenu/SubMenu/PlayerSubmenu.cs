047419af26158bd112afd4bd76ea835fc964829d
local9 <apjcookie1@hotmail.com>
2022-12-11 01:38:01 +0000

    add default messages

--- Curiosity/Curiosity.Framework.Client/Managers/Interface/Menu/InteractiveMenu/SubMenu/PlayerSubmenu.cs
+++ Curiosity/Curiosity.Framework.Client/Managers/Interface/Menu/InteractiveMenu/SubMenu/PlayerSubmenu.cs
@@ -6,8 +6,8 @@ namespace Curiosity.Framework.Client.Managers.Interface.Menu.InteractiveMenu.Sub
     {
         UIMenu _menu;
 
-        UIMenuListItem _menuListMood = new UIMenuListItem("Mood", new List<dynamic>(), 0);
-        UIMenuListItem _menuListWalkingStyle = new UIMenuListItem("Walking Style", new List<dynamic>(), 0);
+        UIMenuListItem _menuListMood = new UIMenuListItem("Mood", new List<dynamic>() { "Loading..." }, 0);
+        UIMenuListItem _menuListWalkingStyle = new UIMenuListItem("Walking Style", new List<dynamic>() { "Loading..." }, 0);
 
         internal void CreateMenu(UIMenu menu)
         {
